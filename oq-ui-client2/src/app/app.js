f655f60069ae940c4d2f87e5d736043db5d3be7f
bwyss <bmwyss@gmail.com>
2012-08-31 13:20:46 +0200

    add the sdk client customised to reflect the ICS viewer app

---
+++ oq-ui-client2/src/app/app.js
@@ -0,0 +1,139 @@
+/**
+ * Add all your dependencies here.
+ *
+ * @require widgets/Viewer.js
+ * @require plugins/LayerTree.js
+ * @require plugins/OLSource.js
+ * @require plugins/OSMSource.js
+ * @require plugins/WMSCSource.js
+ * @require plugins/ZoomToExtent.js
+ * @require plugins/NavigationHistory.js
+ * @require plugins/Zoom.js
+ * @require plugins/AddLayers.js
+ * @require plugins/RemoveLayer.js
+ * @require RowExpander.js
+ * @require plugins/LayerProperties.js
+ * @require widgets/WMSLayerPanel.js
+ * @require plugins/WMSGetFeatureInfo.js
+ * @require plugins/GoogleSource.js
+ * @require plugins/GoogleGeocoder.js
+ */
+
+var app;
+Ext.onReady(function() {
+    app = new gxp.Viewer({
+        portalConfig: {
+            layout: "border",
+            region: "center",
+            
+            // by configuring items here, we don't need to configure portalItems
+            // and save a wrapping container
+            items: [{
+                id: "centerpanel",
+                xtype: "panel",
+                layout: "fit",
+                region: "center",
+                border: false,
+                items: ["mymap"]
+            }, {
+                id: "westpanel",
+                xtype: "container",
+                layout: "fit",
+                region: "west",
+                width: 200
+            }],
+            bbar: {id: "mybbar"}
+        },
+        
+        // configuration of all tool plugins for this application
+        tools: [{
+            ptype: "gxp_layertree",
+            outputConfig: {
+                id: "tree",
+                border: true,
+                tbar: [] // we will add buttons to "tree.bbar" later
+            },
+            outputTarget: "westpanel"
+        }, {
+            ptype: "gxp_googlegeocoder",
+            outputTarget: "map.tbar",
+            outputConfig: {
+                emptyText: "Search for a location ..."
+            }
+        }, {
+            ptype: "gxp_layerproperties",
+            actionTarget: {target: "tree.tbar"},
+        }, {
+            ptype: "gxp_wmsgetfeatureinfo",
+            actionTarget: "tree.tbar",
+            format: "grid", 
+            outputConfig: {
+                width: 400
+            }
+        }, {
+            ptype: "gxp_addlayers",
+            actionTarget: "tree.tbar"
+        }, {
+            ptype: "gxp_removelayer",
+            actionTarget: ["tree.tbar", "tree.contextMenu"]
+        }, {
+            ptype: "gxp_zoomtoextent",
+            actionTarget: "map.tbar"
+        }, {
+            ptype: "gxp_zoom",
+            actionTarget: "map.tbar"
+        }, {
+            ptype: "gxp_navigationhistory",
+            actionTarget: "map.tbar"
+        }],
+        
+        // layer sources
+        sources: {
+            local: {
+                ptype: "gxp_wmscsource",
+                url: "/geoserver/wms",
+                version: "1.1.1"
+            },
+            osm: {
+                ptype: "gxp_osmsource"
+            },
+            google: {
+                ptype: "gxp_googlesource"
+            }
+        },
+        
+        // map and layers
+        map: {
+            id: "mymap", // id needed to reference map in portalConfig above
+            title: "Map",
+            projection: "EPSG:900913",
+            center: [-10764594.758211, 4523072.3184791],
+            zoom: 0,
+            layers: [{
+                source: "google",
+                title: "Google Terrain",
+                name: "TERRAIN",
+                group: "background"
+            }, {
+                source: "google",
+                title: "Google Hybrid",
+                name: "HYBRID",
+                group: "background"
+            }, {
+                source: "osm",
+                name: "mapnik",
+                group: "background"
+            }, {
+                source: "local",
+                name: "isc_viewer:isc_viewer_measure",
+                title : "Instrumental Catalog"
+            }],
+            items: [{
+                xtype: "gx_zoomslider",
+                vertical: true,
+                height: 100
+            }]
+        }
+
+    });
+});
