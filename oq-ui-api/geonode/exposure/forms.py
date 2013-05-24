2f716b203b6f59baaa94a4523a37c039a4cbd9b9
Lars Butler <Lars.Butler@gmail.com>
2013-05-24 09:58:54 +0000

    oq-ui-api/geonode/exposure/forms.py:
    Complete rewrite of exposure export form.

--- oq-ui-api/geonode/exposure/forms.py
+++ oq-ui-api/geonode/exposure/forms.py
@@ -1,32 +1,43 @@
 from django import forms
 
-admin_level0 = [['admin0','Admin Level 0']]
-admin_level1 = [['admin0','Admin Level 0'],['admin1','Admin Level 1']]
-admin_level2 = [['admin0','Admin Level 0'],['admin1','Admin Level 1'],['admin2', 'Admin Level 2']]
-admin_level3 = [['admin0','Admin Level 0'],['admin1','Admin Level 1'],['admin2', 'Admin Level 2'],['admin3', 'Admin Level 3']]
-admin_level4 = [['admin0','Admin Level 0'],['admin1','Admin Level 1'],['admin2', 'Admin Level 2'],['admin3', 'Admin Level 3'],['admin4', 'Admin Level 4']]
-admin_level5 = [['admin0','Admin Level 0'],['admin1','Admin Level 1'],['admin2', 'Admin Level 2'],['admin3', 'Admin Level 3'],['admin4', 'Admin Level 4'],['admin5', 'Admin Level 5']]
+admin_level0 = [['admin0', 'Admin Level 0']]
+admin_level1 = [['admin0', 'Admin Level 0'], ['admin1', 'Admin Level 1']]
+admin_level2 = [['admin0', 'Admin Level 0'], ['admin1', 'Admin Level 1'],
+                ['admin2', 'Admin Level 2']]
+admin_level3 = [['admin0', 'Admin Level 0'], ['admin1', 'Admin Level 1'],
+                ['admin2', 'Admin Level 2'], ['admin3', 'Admin Level 3']]
+admin_level4 = [['admin0', 'Admin Level 0'], ['admin1', 'Admin Level 1'],
+                ['admin2', 'Admin Level 2'], ['admin3', 'Admin Level 3'],
+                ['admin4', 'Admin Level 4']]
+admin_level5 = [['admin0', 'Admin Level 0'], ['admin1', 'Admin Level 1'],
+                ['admin2', 'Admin Level 2'], ['admin3', 'Admin Level 3'],
+                ['admin4', 'Admin Level 4'], ['admin5', 'Admin Level 5']]
 
-class ExposureAdmin0(forms.Form):
-    adminLevel = forms.ChoiceField(widget=forms.RadioSelect(), choices=admin_level0)
+ADMIN_LEVELS = [admin_level0, admin_level1, admin_level2,
+                admin_level3, admin_level4, admin_level5]
 
-class ExposureAdmin1(forms.Form):
-    adminLevel = forms.ChoiceField(widget=forms.RadioSelect(), choices=admin_level1)
+TOD = [['day', 'Day'], ['night', 'Night'], ['transit', 'Transit'], ['all', 'All'], ['off', 'Off']]
+res = [['res', 'Residential'], ['non-res', 'Non-Residential'], ['both', 'Both']]
+OUTPUT_TYPES = [('csv', 'CSV'), ('nrml', 'NRML')]
 
-class ExposureAdmin2(forms.Form):
-    adminLevel = forms.ChoiceField(widget=forms.RadioSelect(), choices=admin_level2)
 
-class ExposureAdmin3(forms.Form):
-    adminLevel = forms.ChoiceField(widget=forms.RadioSelect(), choices=admin_level3)
+class ExposureExportForm(forms.Form):
+    adminLevel = forms.Field()
+    timeOfDay = forms.ChoiceField(widget=forms.RadioSelect(), choices=TOD)
+    residential = forms.ChoiceField(widget=forms.RadioSelect(), choices=res)
+    outputType = forms.ChoiceField(widget=forms.RadioSelect(),
+                                   choices=OUTPUT_TYPES)
 
-class ExposureAdmin4(forms.Form):
-    adminLevel = forms.ChoiceField(widget=forms.RadioSelect(), choices=admin_level4)
+    def __init__(self, *args, **kwargs):
+        highest_admin_level = kwargs.pop('highest_admin_level')
 
-class ExposureAdmin5(forms.Form):
-    adminLevel = forms.ChoiceField(widget=forms.RadioSelect(), choices=admin_level5)
+        super(ExposureExportForm, self).__init__(*args, **kwargs)
 
-TOD = [['day','Day'],['night','Night'],['transit','Transit'],['all', 'All'],['off','Off']]
-res = [['res', 'Residential'],['non-res','Non-Residential'],['both','Both']]
+        self.fields['adminLevel'] = forms.ChoiceField(
+            widget=forms.RadioSelect(),
+            choices=ADMIN_LEVELS[highest_admin_level],
+        )
+
 
 class ExposureTOD(forms.Form):
     timeOfDay = forms.ChoiceField(widget=forms.RadioSelect(), choices=TOD)
