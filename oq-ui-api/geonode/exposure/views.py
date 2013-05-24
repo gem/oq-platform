25daead897c1290f4ec82ca7816d1f60f6229c3c
Lars Butler <Lars.Butler@gmail.com>
2013-05-24 10:09:39 +0000

    oq-ui-api/geonode/exposure/views.py:
    Massive cleanup, renamed exposure export-related views, updated queries
    and removed some hard-coded values, whitespace cleanup, pep8, pyflakes,
    etc.

--- oq-ui-api/geonode/exposure/views.py
+++ oq-ui-api/geonode/exposure/views.py
@@ -18,95 +18,93 @@
 
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_exempt, csrf_response_exempt
-from django.http import HttpResponseRedirect, HttpResponse, HttpResponseBadRequest
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+from django.http import HttpResponseBadRedirect
 from django.contrib.contenttypes.models import ContentType
 from django.utils import simplejson
 from django.views.decorators.http import condition
 from django.db import connections
-#from exposure.template import .......
-from forms import ExposureAdmin0, ExposureAdmin1, ExposureAdmin2, ExposureAdmin3, ExposureAdmin4, ExposureAdmin5, ExposureTOD
+from forms import ExposureExportForm
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.shortcuts import render_to_response
-
-from libs import render_to_wizard
 
 import cStringIO as StringIO
 import csv
 import time
 
+
 @csrf_exempt
-def exposure_form(request):
-        #debug*******
-        #import pdb; pdb.set_trace()
-        #debug*******
+def get_exposure_export_form(request):
+    #debug*******
+    #import pdb; pdb.set_trace()
+    #debug*******
 
-        if request.method =='POST':
-           	# get the lat long variables from the client
-                lat1 = request.POST['lat1']
-            	lng1 = request.POST['lng1']
-            	lat2 = request.POST['lat2']
-            	lng2 = request.POST['lng2']
-                #form = ExposureForm()
+    if request.method == 'POST':
+        # get the lat long variables from the client
+        lat1 = request.POST['lat1']
+        lng1 = request.POST['lng1']
+        lat2 = request.POST['lat2']
+        lng2 = request.POST['lng2']
+        #form = ExposureForm()
 
-                #find all the admin levels available inside bounding box
-                cursor = connections['geddb'].cursor()
-                cursor.execute("""
-			SELECT
-			MIN( 
-			CASE
-			WHEN gadm_admin_3_id IS NOT NULL THEN 3
-			WHEN gadm_admin_2_id IS NOT NULL THEN 2
-			WHEN gadm_admin_1_id IS NOT NULL THEN 1
-			ELSE 0 
-			END)
-			FROM ged2.grid_point WHERE the_geom && ST_MakeEnvelope
-			    (%s, %s, %s, %s, 4326);""", [lng1, lat1, lng2, lat2])
-                admin_level_flag = cursor.fetchall()
+        #find all the admin levels available inside bounding box
+        cursor = connections['geddb'].cursor()
+        cursor.execute("""
+            SELECT
+            MIN(
+            CASE
+            WHEN gadm_admin_3_id IS NOT NULL THEN 3
+            WHEN gadm_admin_2_id IS NOT NULL THEN 2
+            WHEN gadm_admin_1_id IS NOT NULL THEN 1
+            ELSE 0
+            END)
+            FROM ged2.grid_point WHERE the_geom && ST_MakeEnvelope
+                (%s, %s, %s, %s, 4326);""", [lng1, lat1, lng2, lat2])
+        admin_level_flag = cursor.fetchall()
 
-                result = []
+        result = []
 
-		#print some stfuff
+        #print some stfuff
                 #html = '<html><body>%s</body></html>'
-		#return HttpResponse(html % test_table)
+        #return HttpResponse(html % test_table)
 
-		admin_level = admin_level_flag[0][0]
+        admin_level = admin_level_flag[0][0]
 
-		if admin_level == 0:
-			form = ExposureAdmin0()
-			#print something	
-			#html = '<html><body>%s</body></html>'
-			#return HttpResponse(html % admin_level)
+        # Make sure it is a valid admin admin level.
+        # If not, respond with an 'error' message.
+        # (Valid values are 0-5)
+        if not admin_level in range(6):
+            html = ('<html><body>'
+                    'Your request cannot be processed.'
+                    ' Invalid admin level %s.'
+                    '</body></html>' % admin_level)
+            return HttpResponse(html)
 
-                	return render_to_response('exposure-export-wizard-1.html', {'form': form}, context_instance=RequestContext(request))
-		elif admin_level == 1:
-			form = ExposureAdmin1()
-			return render_to_response('exposure-export-wizard-1.html', {'form': form}, context_instance=RequestContext(request))
-                elif admin_level == 2:
-			form = ExposureAdmin2()
-                        return render_to_response('exposure-export-wizard-1.html', {'form': form}, context_instance=RequestContext(request))
-                elif admin_level == 3:
-			form = ExposureAdmin3()
-                        return render_to_response('exposure-export-wizard-1.html', {'form': form}, context_instance=RequestContext(request))
-                elif admin_level == 4:
-			form = ExposureAdmin4()
-                        return render_to_response('exposure-export-wizard-1.html', {'form': form}, context_instance=RequestContext(request))
-                elif admin_level == 5:
-			form = ExposureAdmin5()
-                        return render_to_response('exposure-export-wizard-1.html', {'form': form}, context_instance=RequestContext(request))
-        else:
-                html = '<html><body>Your request cannot be processed</body></html>'
-                return HttpResponse(html)
+        # if the admin level is okay, display the admin level selection form
+        form = ExposureExportForm(highest_admin_level=admin_level)
+        return render_to_response('exposure-export-wizard-1.html',
+                                  {'form': form},
+                                  context_instance=RequestContext(request))
+    else:
+        html = '<html><body>Not implemented</body></html>'
+        return HttpResponse(html)
 
-def exposure_form2(request):
-  form = ExposureTOD()
-  return render_to_response('exposure-export-wizard-2.html', {'form': form}, context_instance=RequestContext(request))
 
 #disabling etag for streaming
 @condition(etag_func=None)
-def read_pop(request):
+def export_exposure(request):
 
-    output_type = request.GET['output_type']
+    html = '<html><body>%s</body></html>' % str(request.GET)
+    return HttpResponse(html)
+
+    # output_type = request.GET['output_type']
+    # TODO: from request.GET, get:
+    #  * 'outputype'
+    #  * 'residential'
+    #  * 'timeofday'
+    #  * 'adminlevel'
 
     content_disp = None
     mimetype = None
@@ -160,10 +158,13 @@ def stream_response_generator(request, output_type):
             SELECT DISTINCT gadm_country_id
             FROM ged2.grid_point grid
             JOIN ged2.gadm_country gadm ON gadm.id=grid.gadm_country_id
-            WHERE ST_intersects(ST_MakeEnvelope(5.93262, 46.96526, 6.63574, 46.60417, 4326),grid.the_geom) 
+            WHERE ST_intersects(ST_MakeEnvelope(%s, %s, %s, %s, 4326),
+                                grid.the_geom)
         ) country
-        JOIN ged2.geographic_region geo ON country.gadm_country_id=geo.gadm_country_id
-        """)
+        JOIN ged2.geographic_region geo
+            ON country.gadm_country_id=geo.gadm_country_id
+        """, [lng1, lat1, lng2, lat2])
+
     country_reg_codes = cursor.fetchall()
     country_codes = [r[0] for r in country_reg_codes]
     region_codes = [r[1] for r in country_reg_codes]
@@ -172,28 +173,25 @@ def stream_response_generator(request, output_type):
 
     #get the dwelling fractions table
     cursor.execute("""
-	SELECT dv.dwelling_fraction, dg.is_urban, dg.occupancy_id, dg.study_region_id, 
- 	    geo.gadm_country_id, geo.id AS geo_id, dv.building_type
-	FROM ged2.geographic_region geo 
-	JOIN ged2.study_region sr ON sr.geographic_region_id=geo.id
-	JOIN ged2.study s ON s.id=sr.study_id
-	JOIN ged2.distribution_group dg ON dg.study_region_id=sr.id
-	JOIN ged2.distribution_value dv ON dv.distribution_group_id=dg.id
-	WHERE geo.gadm_country_id IN (
-	    SELECT DISTINCT gadm_country_id
-	    FROM ged2.grid_point grid
-	    JOIN ged2.gadm_country gadm ON gadm.id=grid.gadm_country_id
-	    WHERE ST_intersects(ST_MakeEnvelope(%s, %s, %s, %s, 4326),grid.the_geom))
- 	AND dg.occupancy_id=0 AND dg.is_urban;""", [lng1, lat1, lng2, lat2])
+        SELECT
+        dv.building_type, dv.dwelling_fraction, dg.is_urban, dg.occupancy_id,
+            dg.study_region_id, geo.id AS geo_id
+        FROM ged2.geographic_region geo
+        JOIN ged2.study_region sr ON sr.geographic_region_id=geo.id
+        JOIN ged2.study s ON s.id=sr.study_id
+        JOIN ged2.distribution_group dg ON dg.study_region_id=sr.id
+        JOIN ged2.distribution_value dv ON dv.distribution_group_id=dg.id
+        WHERE geo.gadm_country_id IN (%s)
+        AND dg.occupancy_id=0 AND dg.is_urban;""", [country_codes])
 
     df_table = cursor.fetchall()
 
     #get the time of day table
     #TODO make the geographic_region_id dynamic
     cursor.execute("""
-	SELECT night_pop_ratio FROM ged2.pop_allocation 
-	WHERE geographic_region_id = 65
-	AND is_urban AND occupancy_id=0""")
+        SELECT night_pop_ratio FROM ged2.pop_allocation WHERE
+            geographic_region_id = %s AND is_urban AND occupancy_id=0;
+    """, [region_codes])
 
     tod_table = cursor.fetchall()
 
@@ -201,12 +199,11 @@ def stream_response_generator(request, output_type):
 
     #get the population table
     cursor.execute("""
-	SELECT grid.gadm_country_id, grid.the_geom, ST_X(grid.the_geom), 
-	ST_Y(grid.the_geom), grid.pop_value, grid.is_urban, gadm.iso, grid.id
-	FROM ged2.grid_point grid
-	JOIN ged2.gadm_country gadm ON gadm.id=grid.gadm_country_id
-	WHERE ST_intersects(ST_MakeEnvelope(%s, %s, %s, %s, 4326),grid.the_geom)
-	;""", [lng1, lat1, lng2, lat2])
+        SELECT grid.gadm_country_id, grid.is_urban, grid.pop_value,
+            grid.the_geom, gadm.iso, ST_X(grid.the_geom), ST_Y(grid.the_geom)
+        FROM ged2.grid_point grid
+        JOIN ged2.gadm2 gadm ON gadm.id_0=grid.gadm_country_id
+        WHERE grid.gadm_country_id IN (%s);""", [country_codes])
 
     copyright_text = '''\
  Version 1.0 released on 31.01.2013
@@ -235,7 +232,6 @@ def stream_response_generator(request, output_type):
  More information on licensing: http://www.globalquakemodel.org/licensing\n
 '''
 
-
     if output_type == "csv":
         # export csv
         # export copyright
@@ -243,11 +239,12 @@ def stream_response_generator(request, output_type):
         yield copyright
 
         # csv header
-        yield '''ISO, pop_calculated_value, pop_cell_ID, lon, lat, study_region, gadm country id, GEM taxonomy'''
+        yield ('ISO, pop_calculated_value, pop_cell_ID, lon, lat, '
+               'study_region, gadm country id, GEM taxonomy')
         yield "\n"
 
         # csv exposure table
-	for pop in cursor.fetchall():
+        for pop in cursor.fetchall():
             pop_gadm_country_id = pop[0]
             pop_lat = pop[2]
             pop_lon = pop[3]
@@ -264,14 +261,17 @@ def stream_response_generator(request, output_type):
                     df_geo = df[5]
                     df_building_type = df[6]
                     if pop_gadm_country_id == df_gadm_country_id:
-			yield ",".join([
-                            str(pop_iso), 
-			    str(pop_value * tod_night_pop_ratio * df_dwelling_fraction),
-                            str(pop_grid_id), str(pop_lon), str(pop_lat), str(df_study_region),
+                        yield ",".join([
+                            str(pop_iso),
+                            str(pop_value * tod_night_pop_ratio
+                                * df_dwelling_fraction),
+                            str(pop_grid_id),
+                            str(pop_lon),
+                            str(pop_lat),
+                            str(df_study_region),
                             str(df_gadm_country_id), str(df_building_type)
                         ])
                         yield "\n"
-
 
     elif output_type == "nrml":
         # export nrml
@@ -285,16 +285,16 @@ def stream_response_generator(request, output_type):
 
         # nrml exposure file
         for pop in cursor.fetchall():
-	    pop_gadm_country_id = pop[0]
-	    pop_lat = pop[2]
+            pop_gadm_country_id = pop[0]
+            pop_lat = pop[2]
             pop_lon = pop[3]
             pop_value = pop[4]
             pop_iso = pop[6]
             pop_grid_id = pop[7]
             for tod in tod_table:
-		tod_night_pop_ratio = tod[0]
+                tod_night_pop_ratio = tod[0]
                 for df in df_table:
-		    df_dwelling_fraction = df[0]
+                    df_dwelling_fraction = df[0]
                     df_is_urban = df[1]
                     df_study_region = df[3]
                     df_gadm_country_id = df[4]
@@ -316,7 +316,8 @@ def stream_response_generator(request, output_type):
                 </assetDefinition>
         </exposureList>
     </exposureModel>\n''' % (pop_grid_id, df_building_type, pop_lon, pop_lat,
-                             pop_value * tod_night_pop_ratio * df_is_urban, df_building_type))
+                             pop_value * tod_night_pop_ratio * df_is_urban,
+                             df_building_type))
         # for loop ends here
         # finalize the document:
         yield "</nrml>\n"
