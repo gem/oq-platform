0edc2ca3042433c30cbb51a8268af00e03d2a438
Lars Butler <Lars.Butler@gmail.com>
2013-05-27 12:49:41 +0000

    oq-ui-api/geonode/exposure/views.py:
    Broke out queries into separate functions, and a bunch of refactoring.

--- oq-ui-api/geonode/exposure/views.py
+++ oq-ui-api/geonode/exposure/views.py
@@ -150,40 +150,11 @@ def export_exposure(request):
     return response
 
 
-def stream_response_generator(request, output_type):
+def _get_country_and_region_codes(lng1, lat1, lng2, lat2):
     """
-    Stream exposure/population data from the database into a file of the
-    specified ``output_type``.
-
-    :param request:
-        A :class:`django.http.request.HttpRequest` object.
-    :param str output_type:
-        A string indicating the desired output type. Valid values are 'csv'
-        and 'nrml' (XML).
+    Query the GED database and get all of the country codes and region codes
+    for the bouding box given by the input coordinates.
     """
-    # possible values are 'res', 'non-res', or 'both'
-    res_select = request.GET['residential']
-    tod_select = request.GET['timeOfDay']
-    admin_select = request.GET['adminLevel']
-    lat1 = request.GET['lat1']
-    lng1 = request.GET['lng1']
-    lat2 = request.GET['lat2']
-    lng2 = request.GET['lng2']
-
-    if res_select == 'res':
-        occupancy = [0]
-    elif res_select == 'non-res':
-        occupancy = [1]
-    else:
-        # assume that it's 'both'
-        occupancy = [0, 1]
-
-    if output_type not in ('csv', 'nrml'):
-        raise RuntimeError(
-            "Unrecognized output type '%s', only 'nrml' and 'csv' are "
-            "supported" % output_type
-        )
-
     cursor = connections['geddb'].cursor()
     cursor.execute("""
         SELECT country.gadm_country_id, geo.id AS geographic_region_id
@@ -201,77 +172,173 @@ def stream_response_generator(request, output_type):
     country_reg_codes = cursor.fetchall()
     country_codes = [r[0] for r in country_reg_codes]
     region_codes = [r[1] for r in country_reg_codes]
+    return country_codes, region_codes
 
-    ccStr = ', '.join(str(e) for e in country_codes)
 
-    #get the dwelling fractions table
+def _get_dwelling_fractions(country_codes, occupancy):
+    """
+    For the given country codes, return a table (2d list) of the following::
+
+        * building_type
+        * dwelling_fraction
+        * is_urban
+        * study_region_id
+        * gadm_country_id (same as ``country_codes``)
+        * region_id
+
+    :param list occupancy:
+        List containing 0, 1, or both (indicating 0=residential,
+        1=non-residential).
+    """
+    cursor = connections['geddb'].cursor()
     cursor.execute("""
         SELECT
-        dv.building_type, dv.dwelling_fraction, dg.is_urban, dg.occupancy_id,
-            dg.study_region_id, geo.id AS geo_id
-        FROM ged2.geographic_region geo
-        JOIN ged2.study_region sr ON sr.geographic_region_id=geo.id
-        JOIN ged2.study s ON s.id=sr.study_id
-        JOIN ged2.distribution_group dg ON dg.study_region_id=sr.id
-        JOIN ged2.distribution_value dv ON dv.distribution_group_id=dg.id
-        WHERE geo.gadm_country_id IN (%s)
-        AND dg.occupancy_id=0 AND dg.is_urban;""", [country_codes])
+            dist_value.building_type,
+            dist_value.dwelling_fraction,
+            dist_group.is_urban,
+            dist_group.study_region_id,
+            geo_region.gadm_country_id,
+            geo_region.id
+        FROM ged2.geographic_region as geo_region
+        JOIN ged2.study_region AS study_region
+            ON study_region.geographic_region_id = geo_region.id
+        JOIN ged2.distribution_group AS dist_group
+            ON dist_group.study_region_id = study_region.id
+        JOIN ged2.distribution_value AS dist_value
+            ON dist_value.distribution_group_id = dist_group.id
+        WHERE
+            geo_region.gadm_country_id IN %s
+            AND dist_group.occupancy_id IN %s
+            AND dist_group.is_urban
+        ORDER BY geo_region.gadm_country_id;
+        """ % (num_list_to_sql_array(country_codes),
+               num_list_to_sql_array(occupancy)))
 
     df_table = cursor.fetchall()
+    return df_table
 
-    #get the time of day table
-    #TODO make the geographic_region_id dynamic
-    cursor.execute("""
-        SELECT night_pop_ratio FROM ged2.pop_allocation WHERE
-            geographic_region_id IN = (%s)
-            AND is_urban
-            AND occupancy_id IN (%s);
-    """, [', '.join(region_codes),
-          ', '.join(occupancy)])
 
-    tod_table = cursor.fetchall()
+def _get_reg_codes_pop_ratios(region_codes, tod, occupancy):
+    """
+    :param tod:
+        Possible values are::
 
-    result = []
+            * 'day'
+            * 'night'
+            * 'transit'
+            * 'all'
+            * 'off' (pop ratio is 1)
 
-    #get the population table
-    cursor.execute("""
+    :param list occupancy:
+        List containing 0, 1, or both (indicating 0=residential,
+        1=non-residential).
+    :returns:
+        A list of 2-tuples containing (region_code, pop_ratio).
+    """
+    cursor = connections['geddb'].cursor()
+    query = """
+        SELECT
+            geographic_region_id AS region_code,
+            %(pop_ratio_column)s AS pop_ratio
+        FROM ged2.pop_allocation
+        WHERE geographic_region_id IN %%s
+        AND is_urban
+        AND occupancy_id IN %%s;
+    """
+
+    if tod == 'off':
+        # pop_ratio is hard-coded to 1 for each region code
+        return [(rc, 1) for rc in region_codes]
+    elif tod == 'all':
+        query %= {
+            'pop_ratio_column': ('SUM(day_pop_ratio, night_pop_ratio, '
+                                 'transit_pop_ratio)')}
+    elif tod in ('day', 'night', 'transit'):
+        # otherwise, query the database for the population ratio
+        # select the correct pop ratio column, depending on the ``tod``
+        tod_column_map = {
+            'day': 'day_pop_ratio',
+            'night': 'night_pop_ratio',
+            'transit': 'transit_pop_ratio',
+        }
+
+        query %= dict(pop_ratio_column=tod_column_map[tod])
+    else:
+        pass
+        # TODO: invalid ``tod`` parameter
+        # TODO: raise an exception
+
+    cursor.execute(query % (num_list_to_sql_array(region_codes),
+                            num_list_to_sql_array(occupancy)))
+    reg_codes_pop_ratios = cursor.fetchall()
+    return reg_codes_pop_ratios
+
+
+def _get_pop_table(lng1, lat1, lng2, lat2):
+    """
+    """
+    cursor = connections['geddb'].cursor()
+    query = """
         SELECT grid.gadm_country_id, grid.is_urban, grid.pop_value,
             grid.the_geom, gadm.iso, ST_X(grid.the_geom), ST_Y(grid.the_geom)
         FROM ged2.grid_point grid
-        JOIN ged2.gadm2 gadm ON gadm.id_0=grid.gadm_country_id
-        WHERE grid.gadm_country_id IN (%s);""", [country_codes])
+        JOIN ged2.gadm_country gadm ON gadm.id=grid.gadm_country_id
+        WHERE ST_intersects(ST_MakeEnvelope(%s, %s, %s, %s, 4326),
+                            grid.the_geom)
+    """ % (lng1, lat1, lng2, lat2)
+    cursor.execute(query)
 
-    copyright_text = '''\
- Version 1.0 released on 31.01.2013
+    pop_table = cursor.fetchall()
+    return pop_table
 
- Copyright (C) 2013 GEM Foundation
 
- Contributions by: see http://www.globalquakemodel.org/contributors
+def stream_response_generator(request, output_type):
+    """
+    Stream exposure/population data from the database into a file of the
+    specified ``output_type``.
 
- You may use this work under the terms of the CC-BY-NC-SA 3.0 (unported)
- [http://creativecommons.org/licenses/by-nc-sa/3.0/]
+    :param request:
+        A :class:`django.http.request.HttpRequest` object.
+    :param str output_type:
+        A string indicating the desired output type. Valid values are 'csv'
+        and 'nrml' (XML).
+    """
+    if output_type not in ('csv', 'nrml'):
+        raise RuntimeError(
+            "Unrecognized output type '%s', only 'nrml' and 'csv' are "
+            "supported" % output_type
+        )
 
- THE WORK IS PROTECTED BY COPYRIGHT AND/OR OTHER APPLICABLE LAW. INSOFAR
- AS THIS WORK IS PROTECTED BY LAWS THAT NEIGHBOUR OR ARE SIMILARLY RELATED
- TO COPYRIGHT, SUCH AS DATABASE RIGHTS AS INTRODUCED IN EUROPE BY THE
- DIRECTIVE 96/9/EC, YOU ALSO MAY USE THIS WORK UNDER THE TERMS OF
- CC-BY-NC-SA 3.0 (unported).
- [http://creativecommons.org/licenses/by-nc-sa/3.0/]
+    # possible values are 'res', 'non-res', or 'both'
+    res_select = request.GET['residential']
+    tod_select = request.GET['timeOfDay']
+    admin_select = request.GET['adminLevel']
+    lng1 = request.GET['lng1']
+    lat1 = request.GET['lat1']
+    lng2 = request.GET['lng2']
+    lat2 = request.GET['lat2']
 
- ANY USE OF THE WORK OTHER THAN AS AUTHORIZED UNDER THIS LICENSE OR
- DIRECTLY ALLOWED BY THE APPLICABLE LAW IS PROHIBITED.
+    if res_select == 'res':
+        occupancy = [0]
+    elif res_select == 'non-res':
+        occupancy = [1]
+    else:
+        # assume that it's 'both'
+        occupancy = [0, 1]
 
- If you have any questions or if you wish to seek permission to use this
- data beyond what is offered by CC-BY-NC-SA 3.0 (unported), please contact
- the GEM Foundation at: licensing@globalquakemodel.org
+    country_codes, region_codes = _get_country_and_region_codes(lng1, lat1,
+                                                                lng2, lat2)
+    ccStr = ', '.join(str(e) for e in country_codes)
 
- More information on licensing: http://www.globalquakemodel.org/licensing\n
-'''
+    df_table = _get_dwelling_fractions(country_codes, occupancy)
+    reg_codes_pop_ratios = _get_reg_codes_pop_ratios(region_codes, tod_select,
+                                                     occupancy)
+    pop_table = _get_pop_table(lng1, lat1, lng2, lat2)
 
     if output_type == "csv":
         # export csv
         # export copyright
-        copyright = copyright_csv(copyright_text)
+        copyright = copyright_csv(COPYRIGHT_HEADER)
         yield copyright
 
         # csv header
@@ -280,37 +347,42 @@ def stream_response_generator(request, output_type):
         yield "\n"
 
         # csv exposure table
-        for pop in cursor.fetchall():
+        for pop in pop_table:
             pop_gadm_country_id = pop[0]
             pop_value = pop[2]
             pop_grid_id = pop[3]
             pop_iso = pop[4]
             pop_lat = pop[5]
             pop_lon = pop[6]
-            for tod in tod_table:
-                tod_night_pop_ratio = tod[0]
+            for reg_code, pop_ratio in reg_codes_pop_ratios:
                 for df in df_table:
                     df_building_type = df[0]
                     df_dwelling_fraction = df[1]
                     df_is_urban = df[2]
-                    df_study_region = df[4]
-                    if pop_gadm_country_id == df_gadm_country_id:
+                    df_study_region = df[3]
+                    df_gadm_country_id = df[4]
+                    df_region_id = df[5]
+
+                    # only write out results for the current region_id and
+                    # country_id, to avoid writing out incorrect/extra data
+                    if  (df_region_id == reg_code and
+                         df_gadm_country_id == pop_gadm_country_id):
                         yield ",".join([
                             str(pop_iso),
-                            str(pop_value * tod_night_pop_ratio
-                                * df_dwelling_fraction),
+                            str(pop_value * pop_ratio * df_dwelling_fraction),
                             str(pop_grid_id),
                             str(pop_lon),
                             str(pop_lat),
                             str(df_study_region),
-                            str(country_codes), str(df_building_type)
+                            str(df_gadm_country_id),
+                            str(df_building_type)
                         ])
                         yield "\n"
 
     elif output_type == "nrml":
         # export nrml
         # nrml header
-        copyright = copyright_nrml(copyright_text)
+        copyright = copyright_nrml(COPYRIGHT_HEADER)
         yield "<?xml version='1.0' encoding='utf-8'?> \n"
         yield copyright
         yield '''
@@ -318,21 +390,26 @@ def stream_response_generator(request, output_type):
       xmlns:gml="http://www.opengis.net/gml>\n'''
 
         # nrml exposure file
-        for pop in cursor.fetchall():
+        for pop in pop_table:
             pop_gadm_country_id = pop[0]
             pop_value = pop[2]
             pop_grid_id = pop[3]
             pop_iso = pop[4]
             pop_lat = pop[5]
             pop_lon = pop[6]
-            for tod in tod_table:
-                tod_night_pop_ratio = tod[0]
+            for reg_code, pop_ratio in reg_codes_pop_ratios:
                 for df in df_table:
                     df_building_type = df[0]
                     df_dwelling_fraction = df[1]
                     df_is_urban = df[2]
-                    df_study_region = df[4]
-                    if pop_gadm_country_id == df_gadm_country_id:
+                    df_study_region = df[3]
+                    df_gadm_country_id = df[4]
+                    df_region_id = df[5]
+
+                    # only write out results for the current region_id and
+                    # country_id, to avoid writing out incorrect/extra data
+                    if  (df_region_id == reg_code and
+                         df_gadm_country_id == pop_gadm_country_id):
                         yield ('''\
     <exposureModel gml:id="ep">
         <exposureList gml:id="exposure" assetCategory="population">
@@ -348,7 +425,7 @@ def stream_response_generator(request, output_type):
                 </assetDefinition>
         </exposureList>
     </exposureModel>\n''' % (pop_grid_id, df_building_type, pop_lon, pop_lat,
-                             pop_value * tod_night_pop_ratio * df_is_urban,
+                             (pop_value * pop_ratio * df_dwelling_fraction),
                              df_building_type))
         # for loop ends here
         # finalize the document:
