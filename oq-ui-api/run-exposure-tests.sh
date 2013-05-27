843cb951af0cdae826bd16a6b2d7e47da3f6c743
Lars Butler <Lars.Butler@gmail.com>
2013-05-27 16:09:33 +0000

    oq-ui-api/run-exposure-tests.sh:
    Added a script for running exposure tests.

--- oq-ui-api/run-exposure-tests.sh
+++ oq-ui-api/run-exposure-tests.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+# To run tests, you'll need to have the `geonode` python package in your
+# PYTHONPATH (for example, /var/lib/geonode/src/GeoNodePy), as well as the
+# `oq-ui-api/geonode` package.
+# Then, DJANGO_SETTINGS_MODULE="geonode.settings".
+# Finally, active the geonode virtualenv:
+#
+# $ source /var/lib/geonode/bin/activate
+
 python $(which nosetests) "$@" geonode/exposure/tests.py
