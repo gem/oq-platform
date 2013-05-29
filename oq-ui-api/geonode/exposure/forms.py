1dc41b9e17b8af91366e2f1f04072f9b79759f7b
Lars Butler <Lars.Butler@gmail.com>
2013-05-29 14:11:19 +0000

    oq-ui-api/geonode/exposure/forms.py:
    Adjusted rendering of exposure export form widgets.

--- oq-ui-api/geonode/exposure/forms.py
+++ oq-ui-api/geonode/exposure/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.safestring import mark_safe
 
 admin_level0 = [['admin0', 'Admin Level 0']]
 admin_level1 = [['admin0', 'Admin Level 0'], ['admin1', 'Admin Level 1']]
@@ -17,12 +18,29 @@ res = [['res', 'Residential'], ['non-res', 'Non-Residential'],
 OUTPUT_TYPES = [('csv', 'CSV'), ('nrml', 'NRML')]
 
 
+class HorizontalRadioRenderer(forms.RadioSelect.renderer):
+
+    def render(self):
+        return mark_safe(u'\n'.join([u'%s\n' % w for w in self]))
+
+
 class ExposureExportForm(forms.Form):
     adminLevel = forms.Field()
-    timeOfDay = forms.ChoiceField(widget=forms.RadioSelect(), choices=TOD)
-    residential = forms.ChoiceField(widget=forms.RadioSelect(), choices=res)
-    outputType = forms.ChoiceField(widget=forms.RadioSelect(),
-                                   choices=OUTPUT_TYPES)
+    timeOfDay = forms.ChoiceField(
+        label='Time of Day',
+        widget=forms.RadioSelect(renderer=HorizontalRadioRenderer),
+        choices=TOD
+    )
+    residential = forms.ChoiceField(
+        label='Residential',
+        widget=forms.RadioSelect(renderer=HorizontalRadioRenderer),
+        choices=res
+    )
+    outputType = forms.ChoiceField(
+        label='Output Type',
+        widget=forms.RadioSelect(renderer=HorizontalRadioRenderer),
+        choices=OUTPUT_TYPES
+    )
 
     def __init__(self, *args, **kwargs):
         highest_admin_level = kwargs.pop('highest_admin_level')
@@ -30,8 +48,7 @@ class ExposureExportForm(forms.Form):
         super(ExposureExportForm, self).__init__(*args, **kwargs)
 
         self.fields['adminLevel'] = forms.ChoiceField(
-            widget=forms.RadioSelect(),
+            label='Admin Level',
+            widget=forms.RadioSelect(renderer=HorizontalRadioRenderer),
             choices=ADMIN_LEVELS[highest_admin_level],
         )
-
-
