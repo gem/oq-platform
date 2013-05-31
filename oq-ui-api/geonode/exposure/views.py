499467304b7a49b9b1202dff2ddaa96be0f13751
Lars Butler <Lars.Butler@gmail.com>
2013-05-31 14:18:52 +0000

    oq-ui-api/geonode/exposure/views.py:
    Moved a bunch of util functions (related to GED DB queries) to the
    util.py module.

--- oq-ui-api/geonode/exposure/views.py
+++ oq-ui-api/geonode/exposure/views.py
@@ -88,12 +88,6 @@ ADMIN_LEVEL_TO_COLUMN_MAP = {
     'admin3': 'gadm_admin_3_id',
 }
 
-#: Convert a Python list (containing numbers, such as record IDs as integers)
-#: to the format required for a SQL query.
-num_list_to_sql_array = lambda a_list: (
-    '(' + ', '.join(str(x) for x in a_list) + ')'
-)
-
 
 @csrf_exempt
 def get_exposure_export_form(request):
@@ -187,197 +181,6 @@ def export_exposure(request):
     return response
 
 
-def _get_admin_level_ids_region_ids(lng1, lat1, lng2, lat2, admin_level_col):
-    """
-    Query the GED database and get all of the admin level IDs and region IDs
-    for the bouding box given by the input coordinates.
-
-    :param lng1, lat1, lng2, lat2:
-        Longitude and latitude values of the user selected bounding box.
-    :param str admin_level_col:
-        Valid values are 'gadm_country_id', 'gadm_admin_1_id',
-        'gadm_admin_2_id', 'gadm_admin_3_id'
-
-        This determines which column is selected from `ged2.grid_point`.
-    """
-    cursor = connections['geddb'].cursor()
-    cursor.execute("""
-        SELECT country.admin_level_id, geo.id AS geographic_region_id
-        FROM (
-            SELECT DISTINCT %(admin_level_col)s AS admin_level_id
-            FROM ged2.grid_point grid
-            WHERE ST_intersects(ST_MakeEnvelope(%(lng1)s, %(lat1)s,
-                                                %(lng2)s, %(lat2)s, 4326),
-                                grid.the_geom)
-        ) country
-        JOIN ged2.geographic_region geo
-            ON country.admin_level_id=geo.%(admin_level_col)s
-        """ % dict(admin_level_col=admin_level_col,
-                   lng1=lng1,
-                   lat1=lat1,
-                   lng2=lng2,
-                   lat2=lat2))
-
-    country_reg_codes = cursor.fetchall()
-    admin_level_ids = [r[0] for r in country_reg_codes]
-    region_ids = [r[1] for r in country_reg_codes]
-    return admin_level_ids, region_ids
-
-
-def _get_dwelling_fractions(admin_level_ids, occupancy, admin_level_col):
-    """
-    For the given country codes, return a table (2d list) of the following::
-
-        * building_type
-        * dwelling_fraction
-        * study_region_id
-        * gadm_country_id (same as ``admin_level_ids``)
-        * region_id
-        * dist_group.is_urban
-
-    :param list occupancy:
-        List containing 0, 1, or both (indicating 0=residential,
-        1=non-residential).
-    :param str admin_level_col:
-        Valid values are 'gadm_country_id', 'gadm_admin_1_id',
-        'gadm_admin_2_id', 'gadm_admin_3_id'
-
-        This determines which column is selected from `ged2.geographic_region`.
-    """
-    cursor = connections['geddb'].cursor()
-    cursor.execute("""
-        SELECT
-            dist_value.building_type,
-            dist_value.dwelling_fraction,
-            dist_group.study_region_id,
-            geo_region.%(admin_level_col)s,
-            geo_region.id,
-            dist_group.is_urban
-        FROM ged2.geographic_region as geo_region
-        JOIN ged2.study_region AS study_region
-            ON study_region.geographic_region_id = geo_region.id
-        JOIN ged2.distribution_group AS dist_group
-            ON dist_group.study_region_id = study_region.id
-        JOIN ged2.distribution_value AS dist_value
-            ON dist_value.distribution_group_id = dist_group.id
-        WHERE
-            geo_region.%(admin_level_col)s IN %(admin_level_ids)s
-            AND dist_group.occupancy_id IN %(occupancy_ids)s
-        ORDER BY geo_region.%(admin_level_col)s;
-        """
-        % dict(admin_level_col=admin_level_col,
-               admin_level_ids=num_list_to_sql_array(admin_level_ids),
-               occupancy_ids=num_list_to_sql_array(occupancy)))
-
-    df_table = cursor.fetchall()
-    return df_table
-
-
-def _get_reg_codes_pop_ratios(region_ids, tod, occupancy):
-    """
-    :param list region_ids:
-        List of GADM region codes (as ints).
-    :param tod:
-        Possible values are::
-
-            * 'day'
-            * 'night'
-            * 'transit'
-            * 'all'
-            * 'off' (pop ratio is 1)
-
-    :param list occupancy:
-        List containing 0, 1, or both (indicating 0=residential,
-        1=non-residential).
-    :returns:
-        A list of 2-tuples containing (region_code, pop_ratio).
-    """
-    cursor = connections['geddb'].cursor()
-    query = """
-        SELECT
-            geographic_region_id AS region_code,
-            %(pop_ratio_column)s AS pop_ratio,
-            is_urban
-        FROM ged2.pop_allocation
-        WHERE geographic_region_id IN %%s
-        AND occupancy_id IN %%s
-    """
-    if tod not in ('day', 'night', 'transit', 'all', 'off'):
-        msg = ("Invalid time of day: '%s'. Expected 'day', 'night', 'transit',"
-               " 'all', or 'off'")
-        msg %= tod
-        raise ValueError(msg)
-
-    if tod == 'off':
-        # pop_ratio is hard-coded to 1 for each region code
-        return [(rc, 1) for rc in region_ids]
-    elif tod == 'all':
-        query %= {
-            'pop_ratio_column': ('(day_pop_ratio + night_pop_ratio + '
-                                 'transit_pop_ratio)')}
-    elif tod in ('day', 'night', 'transit'):
-        # otherwise, query the database for the population ratio
-        # select the correct pop ratio column, depending on the ``tod``
-        tod_column_map = {
-            'day': 'day_pop_ratio',
-            'night': 'night_pop_ratio',
-            'transit': 'transit_pop_ratio',
-        }
-        query %= dict(pop_ratio_column=tod_column_map[tod])
-
-    reg_codes_pop_ratios = util.exec_query(
-        cursor,
-        query % (num_list_to_sql_array(region_ids),
-                 num_list_to_sql_array(occupancy))
-    )
-    return reg_codes_pop_ratios
-
-
-def _get_pop_table(lng1, lat1, lng2, lat2, admin_level_col):
-    """
-    Given the lon/lat of a bounding box, query the following data from the GED
-    DB::
-
-        * admin_level_id
-        * pop_value
-        * the_geom
-        * iso
-        * longitude
-        * latitude
-        * is_urban
-
-    :param str admin_level_col:
-        Valid values are 'gadm_country_id', 'gadm_admin_1_id',
-        'gadm_admin_2_id', 'gadm_admin_3_id'
-
-        This determines which column is selected from `ged2.grid_point`.
-    """
-    cursor = connections['geddb'].cursor()
-    query = """
-        SELECT
-            grid.%(admin_level_col)s,
-            grid.pop_value,
-            grid.id,
-            gadm.iso,
-            ST_X(grid.the_geom),
-            ST_Y(grid.the_geom),
-            grid.is_urban
-        FROM ged2.grid_point grid
-        JOIN ged2.gadm_country gadm ON gadm.id=grid.gadm_country_id
-        WHERE ST_intersects(ST_MakeEnvelope(%(lng1)s, %(lat1)s,
-                                            %(lng2)s, %(lat2)s, 4326),
-                            grid.the_geom)
-    """ % dict(admin_level_col=admin_level_col,
-               lng1=lng1,
-               lat1=lat1,
-               lng2=lng2,
-               lat2=lat2)
-    cursor.execute(query)
-
-    pop_table = cursor.fetchall()
-    return pop_table
-
-
 def _asset_generator(pop_table, reg_codes_pop_ratios, df_table):
     """
     A helper function which implements the looping logic for contructing
@@ -396,12 +199,13 @@ def _asset_generator(pop_table, reg_codes_pop_ratios, df_table):
         * GEM Taxonomy
 
     :param pop_table:
-        Population data. See :func:`_get_pop_table`.
+        Population data. See :func:`exposure.util._get_pop_table`.
     :param reg_codes_pop_ratios:
         Region codes and population ratios. See
-        :func:`_get_reg_codes_pop_ratios`.
+        :func:`exposure.util._get_reg_codes_pop_ratios`.
     :param df_table:
-        Dwelling fraction data. See :func:`_get_dwelling_fractions`.
+        Dwelling fraction data. See
+        :func:`exposure.util._get_dwelling_fractions`.
     """
     for pop in pop_table:
         pop_admin_level_id = pop[0]
@@ -482,16 +286,18 @@ def stream_response_generator(request, output_type):
                % res_select)
         raise ValueError(msg)
 
-    admin_level_ids, region_ids = _get_admin_level_ids_region_ids(
+    admin_level_ids, region_ids = util._get_admin_level_ids_region_ids(
         lng1, lat1, lng2, lat2, admin_level_col
     )
 
     # Get all of the data we need for the loops below:
-    pop_table = _get_pop_table(lng1, lat1, lng2, lat2, admin_level_col)
-    reg_codes_pop_ratios = _get_reg_codes_pop_ratios(region_ids, tod_select,
-                                                     occupancy)
-    df_table = _get_dwelling_fractions(admin_level_ids, occupancy,
-                                       admin_level_col)
+    pop_table = util._get_pop_table(lng1, lat1, lng2, lat2, admin_level_col)
+    reg_codes_pop_ratios = util._get_reg_codes_pop_ratios(
+        region_ids, tod_select, occupancy
+    )
+    df_table = util._get_dwelling_fractions(
+        admin_level_ids, occupancy, admin_level_col
+    )
 
     if output_type == "csv":
         # export csv
