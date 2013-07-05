b235a1050a22e16e4988a02a32c2b60f0f3caf88
bwyss <bmwyss@gmail.com>
2013-07-05 17:20:12 +0200

    a very basic app that allows one to add layers into a leaflet app from tilestream

---
+++ oq-ui-client2/src/explore/explore.js
@@ -0,0 +1,118 @@
+/*
+   Copyright (c) 2013, GEM Foundation.
+
+      This program is free software: you can redistribute it and/or modify
+      it under the terms of the GNU Affero General Public License as
+      published by the Free Software Foundation, either version 3 of the
+      License, or (at your option) any later version.
+
+      This program is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      GNU Affero General Public License for more details.
+
+      You should have received a copy of the GNU Affero General Public License
+      along with this program.  If not, see <https://www.gnu.org/licenses/agpl.html>.
+*/
+
+var MAX_ZOOM_LEVEL = 16;
+
+var startExploreApp = function() {
+
+    /***************
+     * Base layers *
+     ***************/
+    var GEM_base = new L.tileLayer("http://{s}.tiles.mapbox.com/v3/unhcr.map-8bkai3wa/{z}/{x}/{y}.png",
+                                   {subdomains: ['a', 'b', 'c', 'd'], noWrap: true});
+
+    var baselayer = {
+        'Base Map' : GEM_base
+    };
+
+    /******************
+     * Overlay layers *
+     ******************/
+
+    var overlays = {};
+
+    /***********
+     * The map *
+     ***********/
+    map = L.map('map', {
+        center: [20, 20],
+        zoom: 3,
+        maxZoom: MAX_ZOOM_LEVEL,
+        maxBounds: new L.LatLngBounds(new L.LatLng(-90, -185), new L.LatLng(90, 185)),
+        layers: [GEM_base],
+        attributionControl: false,
+    });
+
+    //L.control.layers(baselayer, overlays).addTo(map);
+
+    // Add Wax support
+    L.wax(map);
+
+    //resize the main and map div
+    var mapFit = function() {
+        var main_height = $(window).height()
+                        - $("#header-wrapper").height()
+                        - $("#footer").height();
+       var map_height = $(window).height()
+                        - $("#header-wrapper").height()
+                        - $("#footer").height()
+                        - $("#tooltip").height();
+ 
+       $('#main').css("height", main_height + "px");
+       $('#map').css("height", map_height + "px");
+       map.invalidateSize(false);
+    };
+
+    $(document).ready(mapFit);
+    $(window).resize(mapFit);
+
+    //layer selection dialog
+    $("#dialog-layers").dialog({
+        autoOpen: false,
+        height: 300,
+        width: 350,
+        modal: true
+    });
+
+    $("#layer-selection").button().click(function() {
+        $("#dialog-layers").dialog("open");
+    });
+
+    //get layer names from tilestream
+    var tileStreamLayer = "";
+    var sel = document.getElementById('tile-list');
+    $(".someSpinnerImage").show();
+    $.getJSON('http://tilestream.openquake.org/api/v1/Tileset',
+    function(json) {
+       for (var i=0; i < json.length; i++) {
+            var tileStreamLayer = json[i].id;
+            var opt = document.createElement('option');
+            opt.innerHTML = tileStreamLayer;
+            opt.value = json[i].id;
+            sel.appendChild(opt);
+        }
+    });
+    
+    var layerControl = L.control.layers(overlays);
+    map.addControl(layerControl);
+
+    $(document).ready(function() {
+        $('#addLayer').click(function() {
+            var e = document.getElementById("tile-list");
+            var selectedLayer = e.options[e.selectedIndex].value;
+            var tileLayer = L.tileLayer('http://tilestream.openquake.org/v2/' 
+                + selectedLayer 
+                + '/{z}/{x}/{y}.png',{opacity: 0.8}); 
+            var name = selectedLayer
+            layerControl.addOverlay(tileLayer, name);
+            map.addLayer(tileLayer);
+        });
+    });
+
+};
+
+$(document).ready(startExploreApp);
