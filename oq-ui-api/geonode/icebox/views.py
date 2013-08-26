41c59a93aebe6549760c592f55364bd1e0ecb042
Lars Butler <Lars.Butler@gmail.com>
2013-08-26 12:19:10 +0200

    icebox:
    Added initial sketch of icebox app.

---
+++ oq-ui-api/geonode/icebox/views.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright (c) 2013, GEM Foundation.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/agpl.html>.
+
+from django.http import HttpResponse
+
+
+def list_results(request):
+    return HttpResponse('list_results')
+
+
+def get_result(request, result_id):
+    return HttpResponse('get_result %s' % result_id)
+
+
+def import_results(request):
+    return HttpResponse('import_results')
