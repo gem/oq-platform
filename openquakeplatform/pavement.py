c4a3e6321896b25c3df89b0860e20fcbfc035a87
Lars Butler <Lars.Butler@gmail.com>
2013-09-12 10:11:44 +0200

    pavement:
    Added copyright statement, since we are making modifications.

--- openquakeplatform/pavement.py
+++ openquakeplatform/pavement.py
@@ -1,6 +1,7 @@
 #########################################################################
 #
 # Copyright (C) 2012 OpenPlans
+# Copyright (c) 2013, GEM Foundation.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
