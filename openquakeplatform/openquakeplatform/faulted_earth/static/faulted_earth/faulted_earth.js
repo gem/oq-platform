163d2ae6cb8236b2f7bcdd1c3cb2a3c751cf532b
Lars Butler <Lars.Butler@gmail.com>
2013-09-26 20:15:39 +0200

    faulted_earth/static/faulted_earth/faulted_earth.js:
    Added explicit footer height (45) and autoscroll. These are all the
    styling changes we need for the app to look nice.
    
    Also cleaned up a bunch of garbage: trailing whitspace and mixed tabs
    and spaces.
    
    I feel like I've been cleaning up vomit all day.

--- openquakeplatform/openquakeplatform/faulted_earth/static/faulted_earth/faulted_earth.js
+++ openquakeplatform/openquakeplatform/faulted_earth/static/faulted_earth/faulted_earth.js
@@ -24,7 +24,7 @@ Ext.Ajax.on('beforerequest', function (conn, options) {
        options.headers = {'X-CSRFToken': Ext.util.Cookies.get('csrftoken')};
      } else {
        options.headers.extend({'X-CSRFToken': Ext.util.Cookies.get('csrftoken')});
-     }                        
+     }
    }
 }, this);
 
@@ -33,38 +33,38 @@ Ext.Ajax.on('beforerequest', function (conn, options) {
         localGeoServerBaseUrl: "/geoserver/",
         authorizedRoles: "ROLE_ADMINISTRATOR",
 
-	/* add a listener for computed fields */
-	listeners: {
-	    featureedit: function(manager) {
-		if (manager.featureLayer.selectedFeatures.length > 0) {
-		    var fid = manager.featureLayer.selectedFeatures[0].fid;
-		    if (fid) {
-			Ext.Ajax.request({
-			    method: "POST",
-			    url: faulted_earth.app_url + 'updatecomputedfields',
-			    params: Ext.encode({ fid: fid }),
-			    success: function(response, opts) {
-				/* reload the features to get the autocomputed fields */
-				manager.featureStore.load();
-			    }
-			});
-		    }
-		}
-	    }
-	},
+    /* add a listener for computed fields */
+    listeners: {
+        featureedit: function(manager) {
+        if (manager.featureLayer.selectedFeatures.length > 0) {
+            var fid = manager.featureLayer.selectedFeatures[0].fid;
+            if (fid) {
+            Ext.Ajax.request({
+                method: "POST",
+                url: faulted_earth.app_url + 'updatecomputedfields',
+                params: Ext.encode({ fid: fid }),
+                success: function(response, opts) {
+                /* reload the features to get the autocomputed fields */
+                manager.featureStore.load();
+                }
+            });
+            }
+        }
+        }
+    },
 
         portalConfig: {
             layout: "border",
             region: "center",
-            
+
             // by configuring items here, we don't need to configure portalItems
             // and save a wrapping container
             items: [{
-		region: "north",
-		autoHeight: true,
-		contentEl: 'header-wrapper',
-		id: 'header-ext'
-	    }, {
+        region: "north",
+        autoHeight: true,
+        contentEl: 'header-wrapper',
+        id: 'header-ext'
+        }, {
                 id: "centerpanel",
                 xtype: "panel",
                 layout: "fit",
@@ -87,69 +87,71 @@ Ext.Ajax.on('beforerequest', function (conn, options) {
                 },
                 items: {
                     title: "Layers",
-		    id: "westpanel"
+            id: "westpanel"
                 },
-		initComponent: function() {
-		    Ext.Panel.prototype.initComponent.call(this);
-		    var west_panel = this;
+        initComponent: function() {
+            Ext.Panel.prototype.initComponent.call(this);
+            var west_panel = this;
 
-		    var defaultFormConfig = { padding: 10 };
-		    Ext.each(faulted_earth.models, function(model) {
-			var formConfig = Ext.apply({}, {
-			    id: model.formId,
-			    title: model.formTitle
-			}, defaultFormConfig);
-    
-			west_panel.add(formConfig);
-		    });
-		}
-	    }, {
+            var defaultFormConfig = { padding: 10 };
+            Ext.each(faulted_earth.models, function(model) {
+            var formConfig = Ext.apply({}, {
+                id: model.formId,
+                title: model.formTitle
+            }, defaultFormConfig);
+
+            west_panel.add(formConfig);
+            });
+        }
+        }, {
                 id: "tabs",
                 region: "south",
                 border: false,
                 split: true,
-		autoHeight: true,
+        autoHeight: true,
                 collapseMode: "mini",
                 items: [{
-		    xtype: "tabpanel",
-		    animCollapse: true,
+            xtype: "tabpanel",
+            animCollapse: true,
                     height: 200,
-		    activeTab : 0,
-		    border: true,
-		    initComponent: function() {
-			Ext.TabPanel.prototype.initComponent.call(this);
-			var tabContainer = this;
+            activeTab : 0,
+            border: true,
+            initComponent: function() {
+            Ext.TabPanel.prototype.initComponent.call(this);
+            var tabContainer = this;
 
-			var defaultTabConfig = {
-			    layout: "fit",
-			    height: 180,
-			    autoScroll: true
-			};
-			
-			Ext.each(faulted_earth.models, function(model) {
-			    var tabConfig = {
-				title: model.title,
-				items: Ext.apply({}, { id: model.gridId }, defaultTabConfig)
-			    };
-			    tabContainer.add(tabConfig);
-			});
-		    }
-		}, {
-		    region: "south",
-		    autoHeight: true,
-		    contentEl: 'footer'
-		}]}],
+            var defaultTabConfig = {
+                layout: "fit",
+                height: 180,
+                autoScroll: true
+            };
+
+            Ext.each(faulted_earth.models, function(model) {
+                var tabConfig = {
+                title: model.title,
+                items: Ext.apply({}, { id: model.gridId }, defaultTabConfig)
+                };
+                tabContainer.add(tabConfig);
+            });
+            }
+        }, {
+            region: "south",
+            height: 45,
+            contentEl: 'footer'
+        }]}],
             bbar: {id: "mybbar"}
         },
-        
+
         // configuration of all tool plugins for this application
         tools: [{
             ptype: "gxp_layertree",
             outputConfig: {
                 id: "tree",
                 border: true,
-                tbar: [] // we will add buttons to "tree.bbar" later
-            },
+                // we will add buttons to "tree.bbar" later
+                tbar: [],
+                autoScroll: true,
+        },
             outputTarget: "westpanel"
         },
                 /* commented out as gxp_googlegeocoder is not available with
@@ -168,15 +170,15 @@ Ext.Ajax.on('beforerequest', function (conn, options) {
         }, {
             ptype: "gxp_legend",
             actionTarget: "tree.tbar",
-	    outputTarget: "west",
-	    outputConfig: {
-		title: "Legend",
-		autoScroll: true
-	    }
+        outputTarget: "west",
+        outputConfig: {
+        title: "Legend",
+        autoScroll: true
+        }
         }, {
             ptype: "gxp_wmsgetfeatureinfo",
             actionTarget: "map.tbar",
-            format: "grid", 
+            format: "grid",
             outputConfig: {
                 width: 400
             }
@@ -200,136 +202,136 @@ Ext.Ajax.on('beforerequest', function (conn, options) {
             actionTarget: ["map.tbar"],
             toggleGroup: "main"
         }],
-        
-	initTools: function() {
-	    var viewer = this;
 
-	    var defaultManagerConfig = {
-		    autoLoadFeatures: true,
-		    autoSetLayer: false,
-		    paging: false,
-		    maxFeatures: 1000
-	    };
+    initTools: function() {
+        var viewer = this;
 
-	    var defaultGridConfig = {
-		    alwaysDisplayOnMap: true,
-		    selectOnMap: true,
-		    displayMode: "selected",
-		    controlOptions: {
-		      multiple: true
-		    }
-	    };
+        var defaultManagerConfig = {
+            autoLoadFeatures: true,
+            autoSetLayer: false,
+            paging: false,
+            maxFeatures: 1000
+        };
 
-	    var defaultEditorConfig = {
-		    autoLoadFeature: false,
-		    createFeatureActionText: "Draw",
-		    editFeatureActionText: "Modify"
-	    };
+        var defaultGridConfig = {
+            alwaysDisplayOnMap: true,
+            selectOnMap: true,
+            displayMode: "selected",
+            controlOptions: {
+              multiple: true
+            }
+        };
 
-	    var defaultFormConfig = {};
+        var defaultEditorConfig = {
+            autoLoadFeature: false,
+            createFeatureActionText: "Draw",
+            editFeatureActionText: "Modify"
+        };
 
-	    Ext.each(faulted_earth.models, function(model) {
-		var gridConfig = Ext.apply({}, {
-		    ptype: model.gridPtype,
-		    id: model.gridId,
-		    featureManager: model.managerId,
-		    outputTarget: model.gridId,
-		    outputConfig: {
-			id: model.gridId,
-			loadMask: true,
-			propertyNames: model.propertyNames
-		    }
-		}, defaultGridConfig);
-		viewer.initialConfig.tools.push(gridConfig);
+        var defaultFormConfig = {};
 
-		if (model.hasForm) {
-		    var formConfig = Ext.apply({}, {
-			ptype: model.formPtype,
-			id: model.formId,
-			layerRecordName: model.prefixId,
-			featureManager: model.managerId,
-			featureEditor: model.editorId,
-			outputTarget: model.formId
-		    }, defaultFormConfig);
-		    viewer.initialConfig.tools.push(formConfig);
+        Ext.each(faulted_earth.models, function(model) {
+        var gridConfig = Ext.apply({}, {
+            ptype: model.gridPtype,
+            id: model.gridId,
+            featureManager: model.managerId,
+            outputTarget: model.gridId,
+            outputConfig: {
+            id: model.gridId,
+            loadMask: true,
+            propertyNames: model.propertyNames
+            }
+        }, defaultGridConfig);
+        viewer.initialConfig.tools.push(gridConfig);
 
-		    var editorConfig = Ext.apply({},
-			{
-			    ptype: model.editorPtype,
-			    id: model.editorId,
-			    readOnly: model.readOnly,
-			    modifyOnly: model.modifyOnly,
-			    featureManager: model.managerId,
-			    actionTarget: model.formTarget,
-			    outputConfig: {
-				propertyNames: model.propertyNames
-			    }
-			}, defaultEditorConfig);
+        if (model.hasForm) {
+            var formConfig = Ext.apply({}, {
+            ptype: model.formPtype,
+            id: model.formId,
+            layerRecordName: model.prefixId,
+            featureManager: model.managerId,
+            featureEditor: model.editorId,
+            outputTarget: model.formId
+            }, defaultFormConfig);
+            viewer.initialConfig.tools.push(formConfig);
 
-		    if (model.supportSnapping) {
-			Ext.apply(editorConfig, { snappingAgent: model.snappingId });
-			viewer.initialConfig.tools.push({
-			    ptype: "gxp_snappingagent",
-			    id: model.snappingId,
-			    targets: [{
-				source: "local",
-				name: model.sourceName
-			    }]
-			                                });
-		    }
-		    viewer.initialConfig.tools.push(editorConfig);
+            var editorConfig = Ext.apply({},
+            {
+                ptype: model.editorPtype,
+                id: model.editorId,
+                readOnly: model.readOnly,
+                modifyOnly: model.modifyOnly,
+                featureManager: model.managerId,
+                actionTarget: model.formTarget,
+                outputConfig: {
+                propertyNames: model.propertyNames
+                }
+            }, defaultEditorConfig);
 
-		    var zoomToFeatureConfig = Ext.apply({}, {
-			ptype: "gxp_zoomtoselectedfeatures",
-			featureManager: model.managerId,
-			actionTarget: model.formTarget,
-			tooltip: "Zoom to selected " + model.title
-		    });
-		    viewer.initialConfig.tools.push(zoomToFeatureConfig);
-		    
-		    var deleteSelectedFeatureConfig = Ext.apply({}, {
-			ptype: "gxp_deleteselectedfeatures",
-			actionTarget: model.formTarget,
-			featureManager: model.managerId,
-			tooltip: "Delete selected " + model.title
-		    });
-		    /* 
-		       do we really want this feature?
-		       viewer.initialConfig.tools.push(deleteSelectedFeatureConfig);
-		    */
+            if (model.supportSnapping) {
+            Ext.apply(editorConfig, { snappingAgent: model.snappingId });
+            viewer.initialConfig.tools.push({
+                ptype: "gxp_snappingagent",
+                id: model.snappingId,
+                targets: [{
+                source: "local",
+                name: model.sourceName
+                }]
+                                            });
+            }
+            viewer.initialConfig.tools.push(editorConfig);
 
-		}
+            var zoomToFeatureConfig = Ext.apply({}, {
+            ptype: "gxp_zoomtoselectedfeatures",
+            featureManager: model.managerId,
+            actionTarget: model.formTarget,
+            tooltip: "Zoom to selected " + model.title
+            });
+            viewer.initialConfig.tools.push(zoomToFeatureConfig);
 
-		var managerConfig = Ext.apply({}, {
-		    id: model.managerId,
-		    layer: {
-			source: "local",
-			name: model.sourceName
-		    },
-		    layerRecordName: model.prefixId,
-		    ptype: model.managerPtype
-		}, defaultManagerConfig);
-		viewer.initialConfig.tools.push(managerConfig);
-	    });
+            var deleteSelectedFeatureConfig = Ext.apply({}, {
+            ptype: "gxp_deleteselectedfeatures",
+            actionTarget: model.formTarget,
+            featureManager: model.managerId,
+            tooltip: "Delete selected " + model.title
+            });
+            /*
+               do we really want this feature?
+               viewer.initialConfig.tools.push(deleteSelectedFeatureConfig);
+            */
 
-	    viewer.map = viewer.mapPanel.map;
+        }
+
+        var managerConfig = Ext.apply({}, {
+            id: model.managerId,
+            layer: {
+            source: "local",
+            name: model.sourceName
+            },
+            layerRecordName: model.prefixId,
+            ptype: model.managerPtype
+        }, defaultManagerConfig);
+        viewer.initialConfig.tools.push(managerConfig);
+        });
+
+        viewer.map = viewer.mapPanel.map;
 
     // loading indicator not present in geonode2
-    //	    viewer.initialConfig.tools.push({ ptype: "gxp_loadingindicator" });
+    //        viewer.initialConfig.tools.push({ ptype: "gxp_loadingindicator" });
 
-	    gxp.Viewer.prototype.initTools.call(this);
+        gxp.Viewer.prototype.initTools.call(this);
 
-	    /* call registerEvents for each tools for fine-grained
-	     * customization */
-	    Ext.iterate(viewer.tools, function(tool) {
-		if (viewer.tools[tool].registerEvents)
-		    viewer.tools[tool].registerEvents(viewer);
+        /* call registerEvents for each tools for fine-grained
+         * customization */
+        Ext.iterate(viewer.tools, function(tool) {
+        if (viewer.tools[tool].registerEvents)
+            viewer.tools[tool].registerEvents(viewer);
 
-		if (viewer.tools[tool].ptype.slice(-7) == 'manager') {
-		    viewer.tools[tool].addListener('exception', faulted_earth.on_exception);
-		}
-	    });
-	},
+        if (viewer.tools[tool].ptype.slice(-7) == 'manager') {
+            viewer.tools[tool].addListener('exception', faulted_earth.on_exception);
+        }
+        });
+    },
 
         // layer sources
         sources: {
@@ -345,7 +347,7 @@ Ext.Ajax.on('beforerequest', function (conn, options) {
                 ptype: "gxp_googlesource"
             }
         },
-        
+
         // map and layers
         map: {
             id: "mymap", // id needed to reference map in portalConfig above
@@ -367,9 +369,9 @@ Ext.Ajax.on('beforerequest', function (conn, options) {
                 source: "osm",
                 name: "mapnik",
                 group: "background"
-            }, 
+            },
             /* FIXME: the following sources should be got
-	     * from faulted_earth.models */
+         * from faulted_earth.models */
             {
                 source: "local",
                 name: "oqplatform:faulted_earth_faultsource",
@@ -410,8 +412,8 @@ Ext.Ajax.on('beforerequest', function (conn, options) {
                 vertical: true,
                 height: 100
             }, {
-		xtype: "gxp_scaleoverlay"
-	    }]
+        xtype: "gxp_scaleoverlay"
+        }]
         }
 
     });
