0414355b0e0b7017bcc95603633791298e19048d
Lars Butler <Lars.Butler@gmail.com>
2013-09-26 18:51:42 +0200

    static/js/hazard_models.js:
    Removed a bunch of duplication and cleaned up the hazard app, based
    on the new app template.

--- openquakeplatform/openquakeplatform/static/js/hazard_models.js
+++ openquakeplatform/openquakeplatform/static/js/hazard_models.js
@@ -1,54 +1,38 @@
 /*
 Copyright (c) 2013, GEM Foundation.
 
-      This program is free software: you can redistribute it and/or modify
-      it under the terms of the GNU Affero General Public License as
-      published by the Free Software Foundation, either version 3 of the
-      License, or (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
 
-      This program is distributed in the hope that it will be useful,
-      but WITHOUT ANY WARRANTY; without even the implied warranty of
-      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-      GNU Affero General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
 
-      You should have received a copy of the GNU Affero General Public License
-      along with this program.  If not, see <https://www.gnu.org/licenses/agpl.html>.
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/agpl.html>.
 */
 
-var MAX_ZOOM_LEVEL = 16;
+var baseMapUrl = (
+    "http://{s}.tiles.mapbox.com/v3/unhcr.map-8bkai3wa/{z}/{x}/{y}.png"
+);
+var app = new OQLeaflet.OQLeafletApp(baseMapUrl);
 
-var startHazardModelApp = function() {
-
-    /***************
-     * Base layers *
-     ***************/
-    var GEM_base = new L.tileLayer("http://{s}.tiles.mapbox.com/v3/unhcr.map-8bkai3wa/{z}/{x}/{y}.png",
-                                   {subdomains: ['a', 'b', 'c', 'd'], noWrap: true});
-
-    var baselayer = {
-        'Base Map' : GEM_base
-    };
+var startCallback = function() {
 
     /******************
      * Overlay layers *
      ******************/
-
     hazard_map_japan_21 = L.tileLayer('http://tilestream.openquake.org/v2/hazard-map-japan-21/{z}/{x}/{y}.png');
-
     hazard_map_japan_22 = L.tileLayer('http://tilestream.openquake.org/v2/hazard-map-japan-22/{z}/{x}/{y}.png');
-
     hazard_map_japan_22_land = L.tileLayer('http://tilestream.openquake.org/v2/hazard-map-japan-22-land/{z}/{x}/{y}.png');
-
     hazard_map_japan_21_land = L.tileLayer('http://tilestream.openquake.org/v2/hazard-map-japan-21-land/{z}/{x}/{y}.png');
-
     hazard_map_japan_21_contour = L.tileLayer('http://tilestream.openquake.org/v2/hazard-map-japan-21-contour/{z}/{x}/{y}.png');
-
     hazard_map_japan_22_contour = L.tileLayer('http://tilestream.openquake.org/v2/hazard-map-japan-22-contour/{z}/{x}/{y}.png');
-
     hazard_map_japan_21_contour_land = L.tileLayer('http://tilestream.openquake.org/v2/hazard-map-japan-21-contour-land/{z}/{x}/{y}.png');
-
     hazard_map_japan_22_contour_land = L.tileLayer('http://tilestream.openquake.org/v2/hazard-map-japan-22-contour-land/{z}/{x}/{y}.png');
-
     hazard_curve_Japan = L.tileLayer('http://tilestream.openquake.org/v2/hazard-curve-japan/{z}/{x}/{y}.png', {
         wax: 'http://tilestream.openquake.org/v2/hazard-curve-japan.json'
     });
@@ -58,15 +42,12 @@ var startHazardModelApp = function() {
     });
 
     hazard_map = L.tileLayer('http://tilestream.openquake.org/v2/hazard-map-points-world/{z}/{x}/{y}.png');
-
     hazard_contour = L.tileLayer('http://tilestream.openquake.org/v2/hazard-map-contour-4/{z}/{x}/{y}.png');
-
     hazard_curve = L.tileLayer('http://tilestream.openquake.org/v2/hazard-curve-world/{z}/{x}/{y}.png', {
         wax: 'http://tilestream.openquake.org/v2/hazard-curve-world.json'
     });
 
     grump_rural = L.tileLayer('http://tilestream.openquake.org/v2/gdal-custom-rural/{z}/{x}/{y}.png');
-
     grump_urban = L.tileLayer('http://tilestream.openquake.org/v2/gdal-custom-urban/{z}/{x}/{y}.png',{opacity: 0.8});
 
     var overlays = {
@@ -87,36 +68,11 @@ var startHazardModelApp = function() {
         "World Hazard Curve - PGA" : hazard_curve,
     };
 
-    /***********
-     * The map *
-     ***********/
-    map = L.map('map', {
-        center: [20, 20],
-        zoom: 3,
-        maxZoom: MAX_ZOOM_LEVEL,
-        maxBounds: new L.LatLngBounds(new L.LatLng(-90, -185), new L.LatLng(90, 185)),
-        layers: [GEM_base],
-        attributionControl: false,
-    });
-
-    L.control.layers(baselayer, overlays).addTo(map);
+    app._createMap();
+    L.control.layers(app.baseLayers, overlays).addTo(app.map);
 
     // Add Wax support
-    L.wax(map);
-
-    var mapFit = function() {
-        var headerHeight = $('#oq-page-header').height();
-        var footerHeight = $('#oq-page-footer').height();
-
-        var clientHeight = window.innerHeight;
-
-        // Resize the map so that everything fits on one page:
-        var mapHeight = (clientHeight - headerHeight - footerHeight) - 10;
-        $('#map').css("height", mapHeight + "px");
-        map.invalidateSize(false);
-    };
-    $(document).ready(mapFit);
-    $(window).resize(mapFit);
+    L.wax(app.map);
 
     /*
      * Sliding side panel animation functions:
@@ -146,4 +102,5 @@ var startHazardModelApp = function() {
     };
     $(document).ready(legendSlidingPanel);
 };
-$(document).ready(startHazardModelApp);
+
+app.initialize(startCallback);
