6279a0ead417f348bcb7a28352aa98a763e9b3c8
Luigi Panzeri <defmacro@gmail.com>
2013-10-24 13:57:22 +0200

    use named urls instead of hardcoding urls in local_settings

--- openquakeplatform/openquakeplatform/utils.py
+++ openquakeplatform/openquakeplatform/utils.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
-from openquakeplatform import local_settings
+from django.core.urlresolvers import reverse
+from django.conf import settings
 
 
 class OQTemplateView(TemplateView):
@@ -14,7 +15,8 @@ class OQTemplateView(TemplateView):
         # At the moment, we just need to get the location of the icebox
         # artifacts. Other icebox URLs may need to be added as well in the
         # future.
-        context['icebox_urls'] = local_settings.ICEBOX_URLS
-        context['oq_engine_server_urls'] = local_settings.OQ_ENGINE_SERVER_URLS
-        context['third_party_urls'] = local_settings.THIRD_PARTY_URLS
+        endpoints = ['artifacts', 'artifacts_import', 'artifact_groups']
+        context['icebox_urls'] = dict(zip(endpoints, map(reverse, endpoints)))
+        context['oq_engine_server_urls'] = settings.OQ_ENGINE_SERVER_URLS
+        context['third_party_urls'] = settings.THIRD_PARTY_URLS
         return context
