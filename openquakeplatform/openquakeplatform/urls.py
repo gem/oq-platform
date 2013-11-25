f6d9d5418c4010fd121138232f1d933c1a4b7793
bwyss <bmwyss@gmail.com>
2013-11-25 15:11:49 +0100

    update the menu, add tools html page

--- openquakeplatform/openquakeplatform/urls.py
+++ openquakeplatform/openquakeplatform/urls.py
@@ -63,6 +63,7 @@ urlpatterns = patterns(
     # Static pages
     url(r'^$', 'geonode.views.index', {'template': 'index.html'}, name='home'),
     url(r'^help/$', TemplateView.as_view(template_name='help.html'), name='help'),
+    url(r'^tools/$', TemplateView.as_view(template_name='tools.html'), name='tools'),
     url(r'^developer/$', TemplateView.as_view(template_name='developer.html'), name='developer'),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
 
