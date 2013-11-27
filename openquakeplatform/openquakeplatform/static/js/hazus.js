72ff6533cdac2da4fbb97ea7ee2188b72818151c
bwyss <bmwyss@gmail.com>
2013-11-27 17:28:42 +0100

    hazus.js - format the data to be used by D3.js and provide a D3.js pie chart

--- openquakeplatform/openquakeplatform/static/js/hazus.js
+++ openquakeplatform/openquakeplatform/static/js/hazus.js
@@ -30,7 +30,7 @@ var app = new OQLeaflet.OQLeafletApp(baseMapUrl);
 var startApp = function() {
 
     $(function() {
-        $( "#dialog" ).dialog({height: 300, position: {at: "right bottom"}});
+        $( "#dialog" ).dialog({height: 350, width: 350, position: {at: "right bottom"}});
     });
 
     app.createMap();
@@ -40,8 +40,8 @@ var startApp = function() {
     layerControl = L.control.layers(app.baseLayers);
 
     map.addControl(layerControl.setPosition("topleft"));
-
     map.panTo(new L.LatLng(38.2, -101.6));
+    map.scrollWheelZoom.enable();
 
     // This layer is used for the visual representation of the data
     var hazus = L.tileLayer('http://tilestream.openquake.org/v2/ged-hazus-level1/{z}/{x}/{y}.png');
@@ -60,100 +60,111 @@ var startApp = function() {
     /////////////// Pie Chart //////////////////
     ////////////////////////////////////////////
 
+    function buildD3PieChart(keys, values) {
 
+        var width = 300,
+        height = 300,
+        radius = Math.min(width, height) / 2 - 10;
+    
+        //var data = d3.range(10).map(Math.random).sort(d3.descending);
+        //console.log(data);
+        
+        var color = d3.scale.category20();
+        
+        var arc = d3.svg.arc()
+            .outerRadius(radius);
+        
+        var pie = d3.layout.pie();
+        
+        var svg = d3.select("#dialog").append("svg")
+            .datum(values)
+            .attr("width", width)
+            .attr("height", height)
+            .append("g")
+            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+        
+        var arcs = svg.selectAll("g.arc")
+            .data(pie)
+            .enter().append("g")
+            .attr("class", "arc");
 
-    var width = 960,
-    height = 500,
-    radius = Math.min(width, height) / 2 - 10;
+         // Add a legend.
+        var legend = svg.selectAll("g.legend")
+            .data(keys)
+            .enter().append("svg:g")
+            .attr("class", "legend")
 
-    var data = d3.range(10).map(Math.random).sort(d3.descending);
-    console.log(data);
-    
-    var color = d3.scale.category20();
-    
-    var arc = d3.svg.arc()
-        .outerRadius(radius);
-    
-    var pie = d3.layout.pie();
-    
-    var svg = d3.select("#dialog").append("svg")
-        .datum(data)
-        .attr("width", width)
-        .attr("height", height)
-        .append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-    
-    var arcs = svg.selectAll("g.arc")
-        .data(pie)
-      .enter().append("g")
-        .attr("class", "arc");
-    
-    arcs.append("path")
-        .attr("fill", function(d, i) { return color(i); })
-      .transition()
-        .ease("bounce")
-        .duration(2000)
-        .attrTween("d", tweenPie)
-      .transition()
-        .ease("elastic")
-        .delay(function(d, i) { return 2000 + i * 50; })
-        .duration(750)
-        .attrTween("d", tweenDonut);
-    
-    function tweenPie(b) {
-      b.innerRadius = 0;
-      var i = d3.interpolate({startAngle: 0, endAngle: 0}, b);
-      return function(t) { return arc(i(t)); };
+        legend.append("svg:line")
+            .attr("class", String)
+            .attr("x2", -28)
+            .attr("y2", 0)
+            .attr("transform", function(d, i) { return "translate(-140," + (i * 20 + 75) + ")"; });
+        
+        legend.append("svg:text")
+            .attr("x", -125)
+            .attr("y", -510)
+            .attr("dy", ".31em")
+            .text("test");
+
+        legend.append("svg:text")
+            .attr("x", -125)
+            .attr("y", -510)
+            .attr("dy", ".31em")
+            .text(function(d) { return d; })
+            .attr("transform", function(d, i) { return "translate(0," + (i * 20 + 584) + ")"; });
+        
+        arcs.append("path")
+            .attr("fill", function(d, i) { return color(i); })
+            .transition()
+            //.ease("bounce")
+            //.duration(2000)
+            .attrTween("d", tweenPie)
+            .transition()
+            //.ease("elastic")
+            //.delay(function(d, i) { return 2000 + i * 50; })
+            //.duration(750)
+            .attrTween("d", tweenDonut);
+        
+        function tweenPie(b) {
+          b.innerRadius = 0;
+          var i = d3.interpolate({startAngle: 0, endAngle: 0}, b);
+          return function(t) { return arc(i(t)); };
+        }
+        
+        function tweenDonut(b) {
+          b.innerRadius = radius * .6;
+          var i = d3.interpolate({innerRadius: 0}, b);
+          return function(t) { return arc(i(t)); };
+        }
     }
-    
-    function tweenDonut(b) {
-      b.innerRadius = radius * .6;
-      var i = d3.interpolate({innerRadius: 0}, b);
-      return function(t) { return arc(i(t)); };
-    }
- 
+
     var utfGridClickEvent = function() {
         utfGrid.on('click', function (e) {
+            $("#dialog").empty();
             
             if (e.data) {
                 var b = e.data.bf_json;
                 var bfClean = b.replace(/[\{\}\/"]/g, "");
                 console.log(bfClean);
 
-                //var foo = bfClean.split(":");
-                //var foo = /:(.+)/g.exec(bfClean);
-                var foo = bfClean.substring(bfClean.indexOf(":") + 1);
-                console.log(foo);
+                var data = eval('({' + bfClean + '})');
+                console.log(data);
+                var keys = [];
+                var values = [];
 
-
-                console.log(e.data);
-                var buildingFractionObj = [];
-
-                for (var d in e.data) {
-                    buildingFractionObj.push(e.data[d]);
+                for (var prop in data) {
+                    keys.push(prop);
+                    values.push(data[prop]);
                 }
-                console.log(buildingFractionObj);
-                var values = new Array;
-                var keys = new Array;   
-
-
-
-                for(var key in buildingFractionObj) {
-                    values = buildingFractionObj[key];
-                }
-                console.log("keys: "+keys);
-                console.log("values: "+values);
            
-                buildD3PieChart();
+                buildD3PieChart(keys, values);
 
                 //document.getElementById('dialog').innerHTML = "<b>" + e.data.name + " </b><br>"+ bfClean;
             } else {
                 //document.getElementById('dialog').innerHTML = 'hover: nothing';
             }
         });
-        utfGrid.on('mouseout', function (e) {
-            //console.log('mouseout: ' + e.data);
-        });
+
     }
     utfGridClickEvent();
 };
