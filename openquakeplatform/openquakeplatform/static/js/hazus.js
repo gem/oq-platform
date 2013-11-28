414c63f3627912fc3cb4df565946d50ea6dcec9e
bwyss <bmwyss@gmail.com>
2013-11-28 11:32:03 +0100

    hazus.js - clean up the legend

--- openquakeplatform/openquakeplatform/static/js/hazus.js
+++ openquakeplatform/openquakeplatform/static/js/hazus.js
@@ -30,7 +30,7 @@ var app = new OQLeaflet.OQLeafletApp(baseMapUrl);
 var startApp = function() {
 
     $(function() {
-        $( "#dialog" ).dialog({height: 600, width: 420, position: {at: "right bottom"}});
+        $( "#dialog" ).dialog({height: 520, width: 430, position: {at: "right bottom"}});
     });
 
     app.createMap();
@@ -150,12 +150,12 @@ var startApp = function() {
         
         var legend = d3.select("#dialog").append("svg")
             .attr("class", "legend")
-            .attr("width", r)
-            .attr("height", r * 2)
+            .attr("width", 400)
+            .attr("height", 40)
             .selectAll("g")
             .data(data)
             .enter().append("g")
-            .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+            .attr("transform", function(d, i) { return "translate(" + i * 30 + ",0)"; });
         
         legend.append("rect")
             .attr("width", 18)
@@ -163,8 +163,8 @@ var startApp = function() {
             .style("fill", function(d, i) { return color(i); });
         
         legend.append("text")
-            .attr("x", 24)
-            .attr("y", 9)
+            .attr("x", 0)
+            .attr("y", 30)
             .attr("dy", ".35em")
             .text(function(d) { return d.label; });
 
