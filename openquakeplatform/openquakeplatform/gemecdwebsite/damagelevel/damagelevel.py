7972ef09dd53c9a89886db4199d17f8f43cb7c17
Matteo Nastasi <nastasi@openquake.org>
2014-01-22 10:00:28 +0000

    fix layout for some secondary view

--- openquakeplatform/openquakeplatform/gemecdwebsite/damagelevel/damagelevel.py
+++ openquakeplatform/openquakeplatform/gemecdwebsite/damagelevel/damagelevel.py
@@ -15,6 +15,7 @@ class BasicPage (Pagebase):
 
     def dispatch(self, request, *args, **kwargs):
         self.preProcessPage(request, **kwargs)
+        self.page_context['page_backlink'] = ''
         self.page_context['page_title'] = 'Add new damage level'
         return self.processSimpleForm(request, appname, modelname, formphotosize, template_name, displayfields, editfields, {'errortemplate': 'errorpage.html', 'foreignkeylinkprefix': '/ecd'})
 
