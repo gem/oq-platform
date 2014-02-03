936c20f49c80abe45b71c69688e14cb7222f1b74
bwyss <bmwyss@gmail.com>
2014-02-03 15:36:03 +0100

    address Pr comment

--- openquakeplatform/openquakeplatform/static/js/hazard_viewer.js
+++ openquakeplatform/openquakeplatform/static/js/hazard_viewer.js
@@ -857,7 +857,6 @@ var startApp = function() {
         curve_coup = [];
 
         /* associative array of curves produced with d3.line */
-
         lat = chartData["lat"];
         lon = chartData["lon"];
         invest_time = chartData["invest_time"];
@@ -865,16 +864,16 @@ var startApp = function() {
         // The imt variable needs to be formated i.e. SA = Spectral Acceleration (g)
         // SA-0.1 = Spectral Acceleration (0.1 s)
         imt = chartData["imt"];
-        if (imt.indexOf("SA-") > -1) {
+        if (imt.indexOf("SA-") == 0 ) {
             var imtValue = imt.substring(imt.indexOf("-") + 1);
             imt = "Spectral Acceleration (" + imtValue + " s)";
-        } else if (imt.indexOf("PGA-") > -1) {
+        } else if (imt.indexOf("PGA-") == 0) {
             var imtValue = imt.substring(imt.indexOf("-") + 1);
             imt = "Peak Ground Acceleration (" + imtValue + " s)";
-        } else if (imt.indexOf("PGV-") > -1) {
+        } else if (imt.indexOf("PGV-") == 0) {
             var imtValue = imt.substring(imt.indexOf("-") + 1);
             imt = "Peak Ground Velocity (" + imtValue + " s)";
-        } else if (imt.indexOf("PGD-") > -1) {
+        } else if (imt.indexOf("PGD-") == 0) {
             var imtValue = imt.substring(imt.indexOf("-") + 1);
             imt = "Peak Ground Displacement (" + imtValue + " s)";
         }
