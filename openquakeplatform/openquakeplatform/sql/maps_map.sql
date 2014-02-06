bc2ec8cffc2dc7b3eeab5cc683c53144b5f1aabc
bwyss <bmwyss@gmail.com>
2014-02-06 14:10:55 +0100

    add fixture for the maps_map table

--- openquakeplatform/openquakeplatform/sql/maps_map.sql
+++
@@ -1,2 +0,0 @@
-insert into openquakeplatform_maps_map (resourcebase_ptr_id, zoom, projection, center_x, center_y, last_modified, popular_count, share_count)
-   Values (1, 3, 'EPSG:900913', 0, 0, '2014-02-04 16:21', 1, 0);
