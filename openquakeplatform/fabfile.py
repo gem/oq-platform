3666651088d9fd5b0f44305f4ff25016d4d318dc
Ben Wyss <ben@openquake.org>
2014-06-09 08:27:28 -0700

    add the oqp_maps app and load fixtures to the fabfile

--- openquakeplatform/fabfile.py
+++ openquakeplatform/fabfile.py
@@ -98,7 +98,7 @@ def baseenv(
     init_start()
 
 APPS_LIST=['isc_viewer', 'faulted_earth', 'ghec_viewer', 'gaf_viewer',
-           'econd', 'weblib', 'gemecdwebsite', 'icebox']
+           'econd', 'weblib', 'gemecdwebsite', 'icebox', 'oqp_maps']
 
 def apps(db_name, db_user, db_pass):
     globs = globals()
@@ -117,6 +117,11 @@ def apps(db_name, db_user, db_pass):
     local('openquakeplatform/bin/oq-gs-builder.sh drop')
     local("openquakeplatform/bin/oq-gs-builder.sh restore 'openquakeplatform/build-gs-tree'")
     local('python manage.py updatelayers')
+    local('python manage.py loaddata openquakeplatform/oqp-maps/fixtures/isc_map_layer.json')
+    local('python manage.py loaddata openquakeplatform/oqp-maps/fixtures/ghec_map_layer.json')
+    local('python manage.py loaddata openquakeplatform/oqp-maps/fixtures/gaf_map_layer.json')
+    local('python manage.py loaddata openquakeplatform/oqp-maps/fixtures/topic_categories.json')
+    local('python manage.py map_title')
 
 def clean(db_name='oqplatform', db_user='oqplatform'):
     with settings(warn_only=True):
