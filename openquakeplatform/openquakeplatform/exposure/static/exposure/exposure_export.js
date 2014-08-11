801bc6a119df287cc239ad807f472c59c81eb76c
Daniele Viganò <daniele@vigano.me>
2014-08-11 17:02:13 +0200

    Removes part of code not present in master anymore

--- openquakeplatform/openquakeplatform/exposure/static/exposure/exposure_export.js
+++ openquakeplatform/openquakeplatform/exposure/static/exposure/exposure_export.js
@@ -22,9 +22,6 @@ var objToUrlParams = function(obj) {
 
 var app = new OQLeaflet.OQLeafletApp(THIRD_PARTY_URLS.leaflet_base_map);
 
-// Remove the top ribbon
-$('#oq-context-ribbon').remove();
-
 var startApp = function() {
     drawnItems = new L.LayerGroup();
     // draw tool
