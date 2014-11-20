17e75f488bf52a4e2c3794ad4f0716d8d0c5cc5f
Ben Wyss <ben@openquake.org>
2014-11-20 16:03:15 +0100

    syntax clean up

--- openquakeplatform/openquakeplatform/static/js/irv_viewer_d3_tree.js
+++ openquakeplatform/openquakeplatform/static/js/irv_viewer_d3_tree.js
@@ -35,14 +35,14 @@
         var margin = {top: 20, right: 120, bottom: 20, left: 30},
             width = 960 - margin.right - margin.left,
             height = 800 - margin.top - margin.bottom;
-    
+
         var i = 0,
             duration = 750,
             root;
-        
+
         var tree = d3.layout.tree()
             .size([height, width]);
-        
+
         var diagonal = d3.svg.diagonal()
             .projection(function(d) { return [d.y, d.x]; });
 
@@ -70,7 +70,7 @@
                 // Get the values of the spinners
                 for (var i = 0; i < pdTempSpinnerIds.length; i++) {
                     pdTempWeights.push($('#'+pdTempSpinnerIds[i]).val());
-                };
+                }
 
                 // Adjust the values into percentages
                 pdTempWeights = pdTempWeights.map(Number);
@@ -82,28 +82,28 @@
                 for (var i = 0; i < pdTempWeights.length; i++) {
                     var tempMath = Math.floor((pdTempWeights[i] * 100) / totalWeights);
                     pdTempWeightsComputed.push(tempMath / 100);
-                };
+                }
 
                 // Uopdate the results back into the spinners and to the d3.js chart
                 for (var i = 0; i < pdTempSpinnerIds.length; i++) {
                     $('#'+pdTempSpinnerIds[i]).spinner("value", pdTempWeightsComputed[i]);
-                };
+                }
 
                 // Upadte the json with new values
                 for (var i = 0; i < pdTempWeightsComputed.length; i++) {
                     updateTreeBranch(pdData, [pdTempIds[i]], pdTempWeightsComputed[i]);
-                };
-                
+                }
+
                 nodeEnter.remove("text");
                 updateD3Tree(pdData);
             });
-        };
+        }
 
         function findTreeBranchInfo(pdData, pdName, pdLevel) {
             // Find out how many elements are in tree branch
             if (pdLevel.some(function(currentValue) {
                 return (pdData.level == currentValue);
-                
+
             })) {
                 pdTempIds.push(pdData.id);
                 createSpinner(pdData.id, pdData.weight, pdData.name);
@@ -132,13 +132,13 @@
             .attr("id", "project-definition-svg")
             .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-        
+
         d3.json(selectedPDef, function() {
             data = JSON.parse(selectedPDef);
             root = data;
             root.x0 = height / 2;
             root.y0 = 0;
-            
+
             function collapse(d) {
                 if (d.children) {
                     d._children = d.children;
@@ -150,31 +150,31 @@
             //root.children.forEach(collapse);
             updateD3Tree(root);
         });
-        
+
         d3.select(self.frameElement).style("height", "800px");
-        
+
         function updateD3Tree(source) {
             // Compute the new tree layout.
             var nodes = tree.nodes(root).reverse(),
                 links = tree.links(nodes);
-        
+
             // Normalize for fixed-depth.
             nodes.forEach(function(d) { d.y = d.depth * 180; });
-        
+
             // Update the nodes…
             var node = svg.selectAll("g.node")
                 .data(nodes, function(d) { return d.id || (d.id = ++i); });
-            
+
             // Enter any new nodes at the parent's previous position.
             nodeEnter = node.enter().append("g")
                 .attr("class", "node")
-                .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
+                .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; });
                 //.on("click", click);
-            
+
             nodeEnter.append("circle")
                 .attr("r", 1e-6)
                 .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
-            
+
             nodeEnter.append("text")
                 .attr("class", (function(d) { return "level-" + d.level; }))
                 //.attr("id", (function(d) { return d.name; }))
@@ -202,7 +202,7 @@
             var nodeUpdate = node.transition()
                 .duration(duration)
                 .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
-            
+
             nodeUpdate.select("circle")
                 .attr("r", function (d) {
                     if (d.weight <= 0.10) {
@@ -234,32 +234,32 @@
                     }
                     else if (d.weight > 0.90 && d.weight <= 100 ) {
                         return 20;
-                    };
+                    }
                 })
                 .style("fill", function(d) {
-                    return d.source ? d.source.linkColor: d.linkColor
-                })
+                    return d.source ? d.source.linkColor: d.linkColor;
+                });
 
 
             nodeUpdate.select("text")
                 .style("fill-opacity", 1);
-            
+
             // Transition exiting nodes to the parent's new position.
             var nodeExit = node.exit().transition()
                 .duration(duration)
                 .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
                 .remove();
-            
+
             nodeExit.select("circle")
                 .attr("r", 1e-6);
-            
+
             nodeExit.select("text")
                 .style("fill-opacity", 1e-6);
-            
+
             // Update the links…
             var link = svg.selectAll("path.link")
                 .data(links, function(d) { return d.target.id; });
-            
+
             // Enter any new links at the parent's previous position.
             link.enter().insert("path", "g")
                 .attr("class", "link")
@@ -267,12 +267,12 @@
                   var o = {x: source.x0, y: source.y0};
                   return diagonal({source: o, target: o});
                 });
-            
+
             // Transition links to their new position.
             link.transition()
                 .duration(duration)
                 .attr("d", diagonal);
-            
+
             // Transition exiting nodes to the parent's new position.
             link.exit().transition()
                 .duration(duration)
@@ -281,7 +281,7 @@
                   return diagonal({source: o, target: o});
                 })
                 .remove();
-            
+
             // Stash the old positions for transition.
             nodes.forEach(function(d) {
                 d.x0 = d.x;
@@ -289,11 +289,11 @@
             });
             if (qt_page){
                 if (typeof pdData !== 'undefined'){
-                    qt_page.json_updated(pdData)
+                    qt_page.json_updated(pdData);
                 }
             }
         }
-        
+
         // Toggle children on click.
         function click(d) {
             if (d.children) {
