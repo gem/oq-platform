a078dda0ce27d67a962de1bce9fb4c108f2fa913
Daniele Viganò <daniele@vigano.me>
2015-01-09 12:24:43 +0100

    Exclude GeoNode in external deps installation during deployment

--- openquakeplatform/bin/deploy.sh
+++ openquakeplatform/bin/deploy.sh
@@ -435,7 +435,7 @@ deps_install () {
     pipsrc="/usr/local/openquake/platform"
     mkdir -p "$pipsrc"
     old_IFS="$IFS"
-    for pkg in $(sed -n '/.*dependency_links = /,/.*\].*/p' setup.py  | sed "s/^[^']\+'//g;s/'.*//g" | head -n -1); do
+    for pkg in $(sed -n '/.*dependency_links = /,/.*\].*/p' setup.py  | sed "s/^[^']\+'//g;s/'.*//g" | head -n -1 | grep -v GeoNode); do
         pip install "$pkg"
     done
     IFS="$old_IFS"
