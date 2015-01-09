84d791122d4700d4d8de3f6f8b0797d2b0ca4de7
Daniele Viganò <daniele@vigano.me>
2015-01-09 10:37:05 +0100

    Bump to 2.0.2

--- openquakeplatform/setup.py
+++ openquakeplatform/setup.py
@@ -55,7 +55,7 @@ setup(
     #        the lifecicle of all the bugfix versions of the repository and pip depends on it.
     #        Follow the same rule for all the other gem dependencies when an update is needed.
     # ATTENTION: Please, do not split following lines, the deploy.sh script manages them automatically.
-    dependency_links = ['http://github.com/gem/geonode/tarball/master#egg=GeoNode-2.0.1gem1',
+    dependency_links = ['http://github.com/gem/geonode/tarball/master#egg=GeoNode-2.0.2gem04',
                         'http://github.com/gem/django-extras/tarball/master#egg=django-extras-0.3',
                         'http://github.com/gem/wadofstuff-django-serializers/tarball/master#egg=wadofstuff-django-serializers-1.1.2',
                         'http://github.com/gem/django-nested-inlines/tarball/0.1.4#egg=django-nested-inlines-0.1.4',
@@ -89,7 +89,7 @@ setup(
         "django-taggit-templatetags==0.4.6dev",
         "django-templatetag-sugar==0.1",
         "fabric",
-        "GeoNode==2.0.1gem1",
+        "GeoNode==2.0.2gem04",
         "geonode-avatar==2.1.1",
         "geonode-user-accounts==1.0.5",
         "gisdata==0.5.4",
