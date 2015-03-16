1f768b5c476662d09bd7997c724e025b0f9f92dd
Ben Wyss <ben@openquake.org>
2015-03-16 09:27:59 +0100

    Remove unused code

--- openquakeplatform/openquakeplatform/static/js/irv_viewer.js
+++ openquakeplatform/openquakeplatform/static/js/irv_viewer.js
@@ -44,35 +44,6 @@ function createIndexSimple(la, riskIndicators) {
     for (var q = 0; q < riskIndicators.length; q++) {
         indicatorChildrenKey.push(riskIndicators[q].field);
     }
-
- // Match each primary indicator with it's respective data value
-    var primaryRiskIndicatorObj = {};
-    // Iterate of the layer attribute features and capture the properties object
-    for (var ic = 0; ic < la.length; ic++) {
-        var tempPropertiesObj = la[ic].properties;
-        // for each layer attribute, iterate over each indicator child key
-        for (var d = 0; d < indicatorChildrenKey.length; d++) {
-            // interate over each of the properties objects
-            for (var o in tempPropertiesObj) {
-                // check for a match between the indicator child key and the properties key
-                if (indicatorChildrenKey[d] == o) {
-                    // if a match is made, capture some information about the feature
-                    // and populate the primary risk indicator object
-                    var tempValue = indicatorChildrenKey[d];
-                    var tempValue2 = tempPropertiesObj[o];
-                    var tempValue3 = tempPropertiesObj[selectedRegion];
-                    if (primaryRiskIndicatorObj[tempValue] == undefined) {
-                        primaryRiskIndicatorObj[tempValue] = tempValue2;
-                        primaryRiskIndicatorObj.district = tempValue3;
-                    } else {
-                        primaryRiskIndicatorObj[tempValue] = primaryRiskIndicatorObj[tempValue] + "," +tempValue2;
-                        primaryRiskIndicatorObj.district = primaryRiskIndicatorObj.district + "," +tempValue3;
-                    }
-                }
-            }
-        }
-    }
-    return primaryRiskIndicatorObj;
 }
 
 
