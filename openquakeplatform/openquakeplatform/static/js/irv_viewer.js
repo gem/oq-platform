d579f46c489e7dbd68d3c63133f896ed7c0eaad0
Ben Wyss <ben@openquake.org>
2015-05-13 10:37:15 +0200

    comment out dev env settings

--- openquakeplatform/openquakeplatform/static/js/irv_viewer.js
+++ openquakeplatform/openquakeplatform/static/js/irv_viewer.js
@@ -847,7 +847,7 @@ var startApp = function() {
                 // file 6
                 //layerMetadataURL = "/catalogue/csw?outputschema=http%3A%2F%2Fwww.isotc211.org%2F2005%2Fgmd&service=CSW&request=GetRecordById&version=2.0.2&elementsetname=full&id=3dc19270-e41a-11e4-9826-0800278c33b4";
                 // Portugal-test
-                layerMetadataURL = "/catalogue/csw?outputschema=http%3A%2F%2Fwww.isotc211.org%2F2005%2Fgmd&service=CSW&request=GetRecordById&version=2.0.2&elementsetname=full&id=871f5f50-f23a-11e4-90e9-0800278c33b4 ";
+                //layerMetadataURL = "/catalogue/csw?outputschema=http%3A%2F%2Fwww.isotc211.org%2F2005%2Fgmd&service=CSW&request=GetRecordById&version=2.0.2&elementsetname=full&id=871f5f50-f23a-11e4-90e9-0800278c33b4 ";
 
                 $.get( layerMetadataURL, function( layerMetadata ) {
                     // Convert XML to JSON
