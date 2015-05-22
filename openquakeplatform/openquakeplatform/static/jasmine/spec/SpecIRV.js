bb28f9cd9295c680cd37a1ff74378daee6da79ff
Ben Wyss <ben@openquake.org>
2015-05-22 09:34:06 +0200

    include mock-ajax.js to irv unit test

--- openquakeplatform/openquakeplatform/static/jasmine/spec/SpecIRV.js
+++ openquakeplatform/openquakeplatform/static/jasmine/spec/SpecIRV.js
@@ -1,5 +1,17 @@
+
+
+beforeEach(function() {
+    jasmine.Ajax.install();
+});
+
+
 describe("Hello world", function() {
   it("says hello", function() {
     expect(helloWorld()).toEqual("Hello world!");
   });
+});
+
+
+afterEach(function() {
+    jasmine.Ajax.uninstall();
 });
