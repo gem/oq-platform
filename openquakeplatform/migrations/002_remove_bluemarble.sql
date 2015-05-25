3a8c56a5fe965e9e344cfd27feb1c7eee7335100
Daniele Viganò <daniele@vigano.me>
2015-05-25 10:42:04 +0200

    Replace the SELECT query with DELETE

--- openquakeplatform/migrations/002_remove_bluemarble.sql
+++ openquakeplatform/migrations/002_remove_bluemarble.sql
@@ -1,3 +1,3 @@
 -- This scripts removes any trace of the bluemarble baselayer from maps
 
-SELECT * FROM maps_maplayer WHERE name = 'bluemarble';
+DELETE FROM maps_maplayer WHERE name = 'bluemarble';
