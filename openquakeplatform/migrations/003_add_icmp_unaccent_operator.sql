b260f0b6b77980856f61c7560960a7b0ab97cdca
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2015-05-26 17:07:15 +0200

    Add migration to create the icompare_unaccent operator

---
+++ openquakeplatform/migrations/003_add_icmp_unaccent_operator.sql
@@ -0,0 +1,19 @@
+DROP EXTENSION IF EXISTS unaccent;
+DROP OPERATOR IF EXISTS =~@ (character varying, character varying);
+DROP FUNCTION IF EXISTS icompare_unaccent(character varying, character varying);
+
+CREATE EXTENSION unaccent;
+CREATE FUNCTION icompare_unaccent(character varying, character varying) RETURNS boolean
+    AS 'SELECT lower(unaccent($1)) LIKE lower(unaccent($2));'
+    LANGUAGE SQL
+    IMMUTABLE
+    RETURNS NULL ON NULL INPUT;
+
+CREATE OPERATOR =~@ (
+    LEFTARG = character varying,
+    RIGHTARG = character varying,
+    PROCEDURE = icompare_unaccent,
+    COMMUTATOR = =~@,
+    NEGATOR = !=~@
+);
+
