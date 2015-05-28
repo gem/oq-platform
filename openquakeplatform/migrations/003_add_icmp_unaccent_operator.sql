918acc64259b75e4e6c8a3ae33c5e38c49a9dc0c
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2015-05-28 13:23:03 +0200

    Remove commutator; use upper instead of lower

--- openquakeplatform/migrations/003_add_icmp_unaccent_operator.sql
+++ openquakeplatform/migrations/003_add_icmp_unaccent_operator.sql
@@ -4,7 +4,7 @@ DROP EXTENSION IF EXISTS unaccent;
 
 CREATE EXTENSION unaccent;
 CREATE FUNCTION icompare_unaccent(character varying, character varying) RETURNS boolean
-    AS 'SELECT lower(unaccent($1)) LIKE lower(unaccent($2));'
+    AS 'SELECT upper(unaccent($1)) LIKE upper(unaccent($2));'
     LANGUAGE SQL
     IMMUTABLE
     RETURNS NULL ON NULL INPUT;
@@ -13,7 +13,6 @@ CREATE OPERATOR =~@ (
     LEFTARG = character varying,
     RIGHTARG = character varying,
     PROCEDURE = icompare_unaccent,
-    COMMUTATOR = =~@,
     NEGATOR = !=~@
 );
 
