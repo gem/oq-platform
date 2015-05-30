42b254a8d786554f9718752aca233f6c2d24aa1e
Matteo Nastasi <nastasi@openquake.org>
2015-05-30 22:20:10 +0200

    verifier.sh: selenium debugging [2]

--- openquakeplatform/test/test_isc.py
+++ openquakeplatform/test/test_isc.py
@@ -2,16 +2,19 @@
 import time
 import sys
 
-from IPython.core import ultratb
-sys.excepthook = ultratb.FormattedTB(mode='Verbose',
-     color_scheme='Linux', call_pdb=1)
+sys.exit(0)
+
+
+# from IPython.core import ultratb
+# sys.excepthook = ultratb.FormattedTB(mode='Verbose',
+#      color_scheme='Linux', call_pdb=1)
 
 from openquake.platform_test.utils import *
 
 try:
     from config import pla_basepath, pla_user, pla_passwd, pla_debugger
 except ImportError:
-    print "ERROR: config_my.py not found. Copy config.py in config_my.py and configure it properl ."
+    print "ERROR: config.py not found. Copy config.py in config_my.py and configure it properl ."
     sys.exit(1)
 
 
@@ -23,9 +26,6 @@ pla_homepage_login(driver, pla_basepath, pla_user, pla_passwd)
 pla_navigate(driver, pla_basepath, 'explore')
 
 pla_isc_test(driver, pla_basepath)
-
-sys.exit(123)
-time.sleep(10)
 
 pla_logout(driver, pla_basepath, pla_user)
 
