2aa0bd7c023b9e7251bcdd450e557c1cd5246190
Matteo Nastasi <nastasi@openquake.org>
2015-05-31 09:46:29 +0200

    verifier.sh: selenium debugging [4]

--- verifier.sh
+++ verifier.sh
@@ -269,13 +269,19 @@ virtualenv --system-site-packages platform-env
 pip install -e openquakeplatform
 cd openquakeplatform
 fab --show=everything bootstrap
-cd test
-export PYTHONPATH=\$(pwd)
-cp config.py.tmpl config.py
 "
-#./test_isc.py >selenium.log 2>&1
-#"
-    ssh -t  $lxc_ip "cd ~/$GEM_GIT_PACKAGE; . platform-env/bin/activate ; cd openquakeplatform ; fab stop"
+# cd test
+# export PYTHONPATH=\$(pwd)
+# cp config.py.tmpl config.py
+# ./test_isc.py >selenium.log 2>&1
+# "
+    ssh -t  $lxc_ip "
+export GEM_SET_DEBUG=$GEM_SET_DEBUG
+set -e
+if [ \$GEM_SET_DEBUG ]; then
+    set -x
+fi
+cd ~/$GEM_GIT_PACKAGE; . platform-env/bin/activate ; cd openquakeplatform ; fab stop"
     echo "_devtest_innervm_run: exit"
 
     return 0
