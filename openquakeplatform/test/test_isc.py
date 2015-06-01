5ffd142d377b76774c74b796986f66d6b2affeee
Matteo Nastasi <nastasi@openquake.org>
2015-06-01 18:41:07 +0200

    selenium: add return status management to selenium test

--- openquakeplatform/test/test_isc.py
+++ openquakeplatform/test/test_isc.py
@@ -14,7 +14,7 @@ except ImportError:
     print "ERROR: config.py not found. Copy config.py in config_my.py and configure it properl ."
     sys.exit(1)
 
-
+failed = False
 # Create a new instance of the Firefox driver
 
 driver = pla_driver("firefox", pla_debugger)
@@ -23,8 +23,17 @@ pla_homepage_login(driver, pla_basepath, pla_user, pla_passwd)
 
 pla_navigate(driver, pla_basepath, 'explore')
 
-pla_isc_test(driver, pla_basepath)
+if pla_isc_test(driver, pla_basepath):
+    print "isc_test: SUCCESS"
+else:
+    print "isc_test: FAILED"
+    failed = True
 
 pla_logout(driver, pla_basepath, pla_user)
 
 driver.quit()
+
+if failed:
+    sys.exit(1)
+else:
+    sys.exit(0)
