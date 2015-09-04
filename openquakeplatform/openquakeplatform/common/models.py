93913d48170881f4d2cee5cd4d9b4016e626bed9
Matteo Nastasi <nastasi@openquake.org>
2015-09-04 14:31:06 +0200

    renamed fake model to allow initialization sql script

--- openquakeplatform/openquakeplatform/common/models.py
+++ openquakeplatform/openquakeplatform/common/models.py
@@ -3,7 +3,7 @@ from django.contrib.gis.db import models
 # NOTE: this is a workaround to load custom sql inside Django
 
 # add_icmp_unaccent_operator.sql
-class AddIcmpUnaccentOperator(models.Model):
+class Add_icmp_unaccent_operator(models.Model):
     dumb = models.IntegerField()
 
     class Meta:
