2a4c8fe7823a02b742bf8aa9154cac2157c9d0bd
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2015-09-28 10:04:05 +0200

    Add field code to Zone

--- openquakeplatform/openquakeplatform/svir/models.py
+++ openquakeplatform/openquakeplatform/svir/models.py
@@ -59,6 +59,9 @@ class ZoneManager(models.Manager):
 
 class Zone(models.Model):
     name = models.CharField(max_length=CHMAX)
+    # for countries, code is None and country_iso becomes the identifier
+    # NOTE: code is not the key or part of a composite key
+    code = models.CharField(max_length=CHMAX, null=True, blank=True)
     country_iso = models.CharField(max_length=3)
     admin_level = models.IntegerField()
     parent_label = models.CharField(max_length=CHMAX, null=True, blank=True)
