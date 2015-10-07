ae7925ece72555d75a93219d843fe8b0b4548720
Matteo Nastasi <nastasi@openquake.org>
2015-10-07 16:47:50 +0200

    ci-vuln: moved and commented how to enable pdb on exceptions in a proper place

--- openquakeplatform/openquakeplatform/test/__init__.py
+++ openquakeplatform/openquakeplatform/test/__init__.py
@@ -4,6 +4,13 @@ import nose
 
 from openquakeplatform.test.platform import Platform
 
+# uncomment to run pdb when untrapped exception is raised
+#
+# from IPython.core import ultratb
+# sys.excepthook = ultratb.FormattedTB(mode='Verbose',
+#      color_scheme='Linux', call_pdb=1)
+
+
 pla = Platform()
 
 
