dd14204e4a3aa65f0ec5c33f02ee20ac325dc7e4
Matteo Nastasi <nastasi@openquake.org>
2015-10-07 16:20:28 +0200

    ci-vuln: specific names imports instead of * from utils.py

--- openquakeplatform/openquakeplatform/test/platform.py
+++ openquakeplatform/openquakeplatform/test/platform.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import StaleElementReferenceException
-from utils import *
+from utils import TimeoutError, NotUniqError, wait_for
 
 
 class Platform(object):
