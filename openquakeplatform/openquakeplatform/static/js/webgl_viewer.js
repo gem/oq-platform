9213c002b238d7691a57c97d109a3823adf8351c
Ben Wyss <ben@openquake.org>
2015-11-02 17:12:19 +0100

    Get layer attributes from GeoServer and setup layer breaks

--- openquakeplatform/openquakeplatform/static/js/webgl_viewer.js
+++ openquakeplatform/openquakeplatform/static/js/webgl_viewer.js
@@ -109,7 +109,96 @@ function setupMapboxGlMap() {
         zoom: 2,
     });
 
+    // Check the URL for layer parameter
+    var urlLayerParameter = location.href;
+    urlLayerParameter = urlLayerParameter.split('webgl_viewer/')[1];
+
+    if (urlLayerParameter) {
+        console.log('urlLayerParameter:');
+        console.log(urlLayerParameter);
+        attributeInfoRequest(urlLayerParameter);
+    }
+
     setTimeout(function() {
         $('#absoluteSpinner').hide();
     }, 2000);
+}
+
+function mapboxGlLayerCreation(layerAttributes) {
+    // Color options for GL map
+    var colorsPalRedSingle = ['#fee5d9', '#fcbba1', '#fc9272', '#fb6a4a', '#de2d26', '#a50f15'];
+    var colorsPalBlueSingle = ['#eff3ff', '#c6dbef', '#9ecae1', '#6baed6', '#3182bd', '#08519c'];
+    var colorsPalGreenSingle = ['#edf8e9', '#c7e9c0', '#a1d99b', '#74c476', '#31a354', '#006d2c'];
+    var colorsPalRedMulti = ['#fef0d9', '#fdd49e', '#fdbb84', '#fc8d59', '#e34a33', '#b30000'];
+    var colorsPalBlueMulti = ['#f1eef6', '#d0d1e6', '#a6bddb', '#74a9cf', '#2b8cbe', '#045a8d'];
+    var colorsPalGreenMulti = ['#edf8fb', '#ccece6', '#99d8c9', '#66c2a4', '#2ca25f', '#006d2c'];
+    // Default color
+    var colorsPal = colorsPalRedSingle;
+
+    // Find the values to create categorized color ramp
+    // First find the min and max vales
+    var minMaxArray = [];
+    for (var i = 0; i < layerAttributes.features.length; i++) {
+        for (var k in layerAttributes.features[i].properties) {
+            minMaxArray.push(layerAttributes.features[i].properties[k]);
+        }
+    }
+
+    // TODO fix this
+    //map.fitBounds(mapboxBoundingBox);
+
+
+    var min = Math.min.apply(null, minMaxArray).toFixed(2);
+    var max = Math.max.apply(null, minMaxArray).toFixed(2);
+    min = (parseFloat(min) - 0.1);
+    // Round up the max
+    max = Math.ceil(max * 10) / 10;
+
+    var breaks = [];
+
+    function getColor() {
+        var interval = (max - min) / 6;
+        var tempStep = min;
+        for (var i = 0; i < 5; i++) {
+            tempStep += interval
+            breaks.push(tempStep);
+        }
+        breaks.unshift(min);
+        breaks.push(max);
+    }
+
+    getColor();
+
+    console.log('minMaxArray:');
+    console.log(minMaxArray);
+    console.log('breaks:');
+    console.log(breaks);
+
+    map.addSource('projectSource', {
+        'type': 'geojson',
+        'data': layerAttributes,
+    });
+
+
+}
+
+// Get layer attributes from GeoServer
+function attributeInfoRequest(selectedLayer) {
+    return $.ajax({
+        type: 'get',
+        url: '/geoserver/oqplatform/ows?service=WFS&version=1.0.0&request=GetFeature&typeName='+ selectedLayer +'&outputFormat=json',
+        success: function(data) {
+            layerAttributes = data;
+            console.log('layerAttributes:');
+            console.log(layerAttributes);
+            mapboxGlLayerCreation(layerAttributes);
+        },
+        error: function() {
+            $('#ajaxErrorDialog').empty();
+            $('#ajaxErrorDialog').append(
+                '<p>This application was not able to get information about the selected layer</p>'
+            );
+            $('#ajaxErrorDialog').dialog('open');
+        }
+    });
 }
