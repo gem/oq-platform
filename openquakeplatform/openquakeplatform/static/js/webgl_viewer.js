bbca515de26a7023032a9b11bc6d6ec27b5f7337
Ben Wyss <ben@openquake.org>
2015-11-05 13:47:41 +0100

    Create the map legend

--- openquakeplatform/openquakeplatform/static/js/webgl_viewer.js
+++ openquakeplatform/openquakeplatform/static/js/webgl_viewer.js
@@ -27,19 +27,19 @@ $(document).ready(function() {
     // Create an input field for the heatmap intensity option
     // TODO clean up css
     $('#map-tools').append(
-        '<div id="intensity-div" style="float: left;">'+
-            '<label for="heatmap-intensity-value" style="float: left; margin-left: 10px; margin-top: 4px;">Heatmap Intensity:</label>'+
-            '<input type="text" id="heatmap-intensity-value" readonly style="border:0; color:#f6931f; font-weight:bold; width:30px;">'+
-            '<div id="heatmap-intensity" style="width:300px; margin-left: 10px;"></div><br>'+
+        '<div id="intensity-div">'+
+            '<label id="intensity-labels" for="heatmap-intensity-value">Heatmap Intensity:</label>'+
+            '<input type="text" id="heatmap-intensity-value" readonly>'+
+            '<div id="heatmap-intensity"></div><br>'+
         '</div>'
     );
 
     // Create an input field for the heatmap size option
     $('#map-tools').append(
         '<div id="size-div">'+
-            '<label for="heatmap-size-value" style="float: left; margin-left: 40px; margin-top: 4px;">Heatmap Size:</label>'+
-            '<input type="text" id="heatmap-size-value" readonly style="border:0; color:#f6931f; font-weight:bold; width:30px;">'+
-            '<div id="heatmap-size" style="width:300px; margin-left: 350px;"></div>'+
+            '<label id="size-labels" for="heatmap-size-value">Heatmap Size:</label>'+
+            '<input type="text" id="heatmap-size-value" readonly>'+
+            '<div id="heatmap-size"></div>'+
         '</div>'
     );
 
@@ -71,12 +71,13 @@ $(document).ready(function() {
 
     // Calculate the height:
     var mapToolsHeight = $('#map-tools').height();
+    console.log('mapToolsHeight:');
+    console.log(mapToolsHeight);
     var headerHeight = $('#header').height();
     var footerHeight = $('#footer').height();
     var mtoolsHeight = $('#map-tools').height();
     var ribbonHeight = $('#oq-context-ribbon').height();
     var mapHeight = (window.innerHeight - headerHeight - footerHeight - mtoolsHeight - ribbonHeight - mapToolsHeight);
-
 
     // Watch for heatmap intensity update
     $("#heatmap-intensity").on("slidestop", function() {
@@ -120,36 +121,6 @@ $(document).ready(function() {
         return;
     }
 
-    // Check the browser for webGL support
-    function webglDetect(return_context) {
-        if (!!window.WebGLRenderingContext) {
-            var canvas = document.createElement("canvas"),
-                names = ["webgl", "experimental-webgl", "moz-webgl", "webkit-3d"],
-                context = false;
-
-            for(var i=0;i<4;i++) {
-                try {
-                    context = canvas.getContext(names[i]);
-                    if (context && typeof context.getParameter == "function") {
-                        // WebGL is enabled
-                        if (return_context) {
-                            // Return WebGL object if the function's argument is present
-                            return {name:names[i], gl:context};
-                        }
-                        // Else, return just true
-                        return true;
-                    }
-                } catch(e) {}
-            }
-
-            // WebGL is supported, but disabled
-            return false;
-        }
-
-        // WebGL not supported
-        return false;
-    }
-
     webGl = webglDetect();
 
     if (webGl === false) {
@@ -163,6 +134,36 @@ $(document).ready(function() {
         setupLeafletMap();
     }
 });
+
+// Check the browser for webGL support
+function webglDetect(return_context) {
+    if (!!window.WebGLRenderingContext) {
+        var canvas = document.createElement("canvas"),
+            names = ["webgl", "experimental-webgl", "moz-webgl", "webkit-3d"],
+            context = false;
+
+        for(var i=0;i<4;i++) {
+            try {
+                context = canvas.getContext(names[i]);
+                if (context && typeof context.getParameter == "function") {
+                    // WebGL is enabled
+                    if (return_context) {
+                        // Return WebGL object if the function's argument is present
+                        return {name:names[i], gl:context};
+                    }
+                    // Else, return just true
+                    return true;
+                }
+            } catch(e) {}
+        }
+
+        // WebGL is supported, but disabled
+        return false;
+    }
+
+    // WebGL not supported
+    return false;
+}
 
 function setupLeafletMap() {
     // Create a WebGl map
@@ -198,6 +199,8 @@ function setupLeafletMap() {
 
 function mapboxGlLayerCreation(layerAttributes) {
 
+    var allValuesArray = [];
+
     // Format the data to be used by heatmap plugin,
     // array of arrays like this: [[lat, lng, heatmapIntensity]...]
     for (var i = 0; i < layerAttributes.features.length; i++) {
@@ -208,6 +211,8 @@ function mapboxGlLayerCreation(layerAttributes) {
             // TODO fix the hard codded iml element
             layerAttributes.features[i].properties.iml
         );
+        // Push all the mapped values into an array to be used for the legend
+        allValuesArray.push(layerAttributes.features[i].properties.iml);
         dataPoints.push(tmpArray);
     }
 
@@ -225,6 +230,10 @@ function mapboxGlLayerCreation(layerAttributes) {
     var minLat = Math.min.apply(null, minMaxLat).toFixed(2);
     var maxLat = Math.max.apply(null, minMaxLat).toFixed(2);
 
+    // Get the max mapped value
+    var maxMappedValue = Math.max.apply(null, allValuesArray).toFixed(2);
+    var minMappedValue = Math.min.apply(null, allValuesArray).toFixed(2);
+
     // Fit the map to bounding box
     map.fitBounds([[minLat, minLng], [maxLat, maxLng]]);
 
@@ -234,6 +243,8 @@ function mapboxGlLayerCreation(layerAttributes) {
     }, 1000);
 
     createHeatMapLayer();
+
+    createLegend(minMappedValue, maxMappedValue);
 }
 
 function createHeatMapLayer() {
@@ -251,8 +262,6 @@ function createHeatMapLayer() {
         var point = dataPoints[i];
         heatmap.addDataPoint(point[0], point[1], heatmapIntensity);
     }
-
-    // TODO create legend
 
     map.addLayer(heatmap);
 }
@@ -276,4 +285,104 @@ function attributeInfoRequest(selectedLayer) {
     });
 }
 
+function createLegend(minMappedValue, maxMappedValue) {
+    // Create the map legend div
+    $('#map-tools').append(
+        '<div id="mapLegend"></div>'
+    );
+
+    ///////////////////////
+    // Create the legend //
+    ///////////////////////
+
+    var svgWidth = 250,
+        svgHeight = 40,
+        x1 = 0,
+        barWidth = 250,
+        y1 = 0,
+        barHeight = 20,
+        numberHues = 35;
+
+    var idGradient = "legendGradient";
+
+    var svgForLegendStuff = d3.select("#mapLegend").append("svg")
+        .attr("width", svgWidth)
+        .attr("height", svgHeight);
+
+    // Create the empty gradient that we're going to populate later
+    svgForLegendStuff.append("g")
+        .append("defs")
+        .append("linearGradient")
+            .attr("id",idGradient)
+            .attr("x1","0%")
+            .attr("x2","100%")
+            .attr("y1","0%")
+            .attr("y2","0%"); // x1=0, x2=100%, y1=y2 results in a horizontal gradient
+    // Create the bar for the legend to go into
+    // the "fill" attribute hooks the gradient up to this rect
+    svgForLegendStuff.append("rect")
+        .attr("fill","url(#" + idGradient + ")")
+        .attr("x",x1)
+        .attr("y",y1)
+        .attr("width",barWidth)
+        .attr("height",barHeight);
+
+    svgForLegendStuff.append("text")
+        .attr("class","legendText")
+        .attr("text-anchor", "middle")
+        .attr("x",x1 + 15)
+        .attr("y", 35)
+        .attr("dy",0)
+        .text(minMappedValue);
+
+    svgForLegendStuff.append("text")
+        .attr("class","legendText")
+        .attr("text-anchor", "left")
+        .attr("x",x1 + 220)
+        .attr("y", 35)
+        .attr("dy",0)
+        .text(maxMappedValue);
+
+    // We go from a somewhat transparent blue/green (hue = 160º, opacity = 0.3)
+    // to a fully opaque reddish (hue = 0º, opacity = 1)
+    var hueStart = 160, hueEnd = 0;
+    var opacityStart = 0.3, opacityEnd = 1.0;
+    var theHue, rgbString, opacity,p;
+
+    var deltaPercent = 1/(numberHues-1);
+    var deltaHue = (hueEnd - hueStart)/(numberHues - 1);
+    var deltaOpacity = (opacityEnd - opacityStart)/(numberHues - 1);
+
+    // Set up the data here
+    var theData = [];
+    for (var i = 0; i < numberHues; i++) {
+        theHue = hueStart + deltaHue*i;
+        // The second parameter, set to 1 here, is the saturation
+        // The third parameter is "lightness"
+        rgbString = d3.hsl(theHue,1,0.6).toString();
+        opacity = opacityStart + deltaOpacity*i;
+        p = 0 + deltaPercent*i;
+        theData.push({"rgb":rgbString, "opacity":opacity, "percent":p});
+    }
+
+    var stops = d3.select('#' + idGradient).selectAll('stop')
+        .data(theData);
+    stops.enter().append('stop');
+    stops.attr('offset',function(d) {
+        return d.percent;
+    })
+    .attr('stop-color',function(d) {
+        return d.rgb;
+    })
+    .attr('stop-opacity',function(d) {
+        return d.opacity;
+    });
+
+    ////////////////
+    // legend end //
+    ////////////////
+}
+
+// TODO save the state of the intensity and size parameters
+// TODO provide some kinf of 'smart guess' a initial intensity and size parameters
 
