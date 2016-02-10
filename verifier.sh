09d5e289dc0eb1edbb16b3e5355a0a0eeaee4ed5
Matteo Nastasi <nastasi@openquake.org>
2016-02-10 14:03:14 +0000

    verifier: comment updated

--- verifier.sh
+++ verifier.sh
@@ -112,7 +112,7 @@ else
     ffox_can="\$(echo "\$ffox_pol" | grep '^  Candidate:' | sed 's/.*: //g')"
 
     if [ "\$ffox_cur" != "\$ffox_can" ]; then
-        echo "WARNING: firefox is upgraded, run it to accomplish update operations"
+        echo "WARNING: firefox has been upgraded, run it to accomplish update operations"
         sudo apt-get -y upgrade
         sudo apt-get -y install wmctrl
         export DISPLAY=:1
