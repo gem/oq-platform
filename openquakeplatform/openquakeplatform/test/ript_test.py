7e48c65f493720c047114d6bd5d0afe6d00cda60
Matteo Nastasi <nastasi@openquake.org>
2016-03-24 09:32:15 +0000

    ript_test refactored

--- openquakeplatform/openquakeplatform/test/ript_test.py
+++ openquakeplatform/openquakeplatform/test/ript_test.py
@@ -14,14 +14,13 @@ class RiptTest(unittest.TestCase):
         #     class="btn btn-primary">Convert to NRML</button>
 
         convert_btn = pla.xpath_finduniq(
-            "//button[@id='saveBtnEX' and @type='button'"
+            "//div[contains(concat(' ',normalize-space(@class),' '),' ex_gid ')]"
+            "//button[@id='convertBtn' and @type='button']"
             " and normalize-space(text())='Convert to NRML']")
 
-        win_cur = pla.current_window_handle()
         convert_btn.click()
 
-        alert = pla.switch_to_alert()
-        self.assertEquals(
-            alert.text, "whoops, there seem to be some empty cells")
-        alert.accept()
-        pla.switch_to_window(win_cur)
+        result = pla.xpath_finduniq(
+            "//div[contains(concat(' ',normalize-space(@class),' '),' ex_gid ')]"
+            "//div[@id='outputDiv']//div[@id='validationErrorMsg'"
+            " and normalize-space(text())='Validation error: empty cell at coords (1, 1).']")
