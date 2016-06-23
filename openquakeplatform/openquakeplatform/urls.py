f2e10f7b1c03ef8f91d036d9f9c21218a7710fcb
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2016-06-23 17:00:46 +0200

    specify app_name when including urls for apps

--- openquakeplatform/openquakeplatform/urls.py
+++ openquakeplatform/openquakeplatform/urls.py
@@ -90,16 +90,16 @@ urlpatterns = patterns('',
         template_name="hazus/hazus.html"), name='hazus'),
     url(r'^hrde/$', TemplateView.as_view(
         template_name="hrde/hrde.html"), name='hrde'),
-    url(r'^irv/', include('openquakeplatform.irv.urls')),
+    url(r'^irv/', include('openquakeplatform.irv.urls', app_name='irv')),
 
-    url(r'^ipt/', include('openquakeplatform_ipt.urls')),
+    url(r'^ipt/', include('openquakeplatform_ipt.urls', app_name='ipt')),
  
-    (r'^world/', include('openquakeplatform.world.urls')),
-    (r'^faulted_earth/', include('openquakeplatform.faulted_earth.urls')),
-    (r'^exposure/', include('openquakeplatform.exposure.urls')),
-    (r'^svir/', include('openquakeplatform.svir.urls')),
-    (r'^vulnerability/', include('openquakeplatform.vulnerability.urls')),
-    (r'^taxtweb/', include('openquakeplatform.taxtweb.urls')),
+    (r'^world/', include('openquakeplatform.world.urls', app_name='world')),
+    (r'^faulted_earth/', include('openquakeplatform.faulted_earth.urls', app_name='faulted_earth')),
+    (r'^exposure/', include('openquakeplatform.exposure.urls', app_name='exposure')),
+    (r'^svir/', include('openquakeplatform.svir.urls', app_name='svir')),
+    (r'^vulnerability/', include('openquakeplatform.vulnerability.urls', app_name='vulnerability')),
+    (r'^taxtweb/', include('openquakeplatform.taxtweb.urls', app_name='taxtweb')),
 
     # Static pages
     url(r'^$', 'geonode.views.index', {'template': 'index.html'}, name='home'),
