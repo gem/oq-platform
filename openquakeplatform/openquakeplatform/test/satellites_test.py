1203382154aef07c280c09857d72fa6c46f8af71
Matteo Nastasi <nastasi@openquake.org>
2016-07-27 10:46:01 +0200

    add new test file to import external applications tests

---
+++ openquakeplatform/openquakeplatform/test/satellites_test.py
@@ -0,0 +1,23 @@
+import unittest
+from inspect import isclass
+
+def import_by_name(clsname):
+    components = clsname.split('.')
+    leaf = components[-1]
+    mod = __import__(clsname, fromlist=[leaf])
+    return mod
+
+def get_checks(clsname):
+    cls = import_by_name(clsname)
+    for objname in dir(cls):
+         obj = getattr(cls, objname)
+         if isclass(obj):
+             if issubclass(obj, unittest.TestCase):
+                 newname = "%s_%s" % (clsname.replace('.', '_'), objname)
+                 globals()[newname] = obj
+
+for clsname in ['openquakeplatform_ipt.test', 'openquakeplatform_taxtweb.test']:
+    try:
+        get_checks(clsname)
+    except:
+        pass
