9e7b9d39c6d99b7d4c2a99e86ea4cb0e6d8ae941
Matteo Nastasi <nastasi@openquake.org>
2016-11-18 11:04:58 +0100

    add modifications to follow Moon 0.3 version directives

--- openquakeplatform/openquakeplatform/test/__init__.py
+++ openquakeplatform/openquakeplatform/test/__init__.py
@@ -2,21 +2,13 @@ import time
 import sys
 import nose
 
-from openquakeplatform.test.oqplatform import Platform
+from openquake.moon import Moon
 
-# uncomment to run pdb when untrapped exception is raised
-#
-# from IPython.core import ultratb
-# sys.excepthook = ultratb.FormattedTB(mode='Verbose',
-#      color_scheme='Linux', call_pdb=1)
-
-
-pla = Platform()
-
+pla = Moon()
+pla.primary_set()
 
 def setup_package():
     pla.init()
-
 
 def teardown_package():
     pla.fini()
