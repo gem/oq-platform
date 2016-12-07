afaa978d9d4ebda5eeb6816107a204e75dfe4158
Matteo Nastasi <nastasi@openquake.org>
2016-12-07 10:02:29 +0100

    from teardown_package function to atexit registered function

--- openquakeplatform/openquakeplatform/test/__init__.py
+++ openquakeplatform/openquakeplatform/test/__init__.py
@@ -1,4 +1,5 @@
 from openquake.moon import Moon
+import atexit
 
 pla = Moon()
 pla.primary_set()
@@ -6,5 +7,12 @@ pla.primary_set()
 def setup_package():
     pla.init()
 
-def teardown_package():
+# turned off because nose run it at the wrong time
+#def teardown_package():
+#    print "teardown_package here"
+#    pla.fini()
+
+def my_at_exit():
     pla.fini()
+
+atexit.register(my_at_exit)
