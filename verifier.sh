33c7e830e27061bf8a8142561bd95842d4544ea4
Matteo Nastasi <nastasi@openquake.org>
2016-12-12 13:48:28 +0100

    reintroduced devel big sleep

--- verifier.sh
+++ verifier.sh
@@ -438,8 +438,8 @@ python ./manage.py loaddata dev-data.json.bz2
 export PYTHONPATH=\$(pwd):\$(pwd)/../../oq-moon:\$(pwd)/openquakeplatform/test/config
 cp openquakeplatform/test/config/moon_config.py.tmpl openquakeplatform/test/config/moon_config.py
 export DISPLAY=:1
-python -m openquake.moon.nose_runner --failurecatcher dev -v --with-xunit --xunit-file=xunit-platform-dev.xml openquakeplatform/test  # || true
-# sleep 20000 || true
+python -m openquake.moon.nose_runner --failurecatcher dev -v --with-xunit --xunit-file=xunit-platform-dev.xml openquakeplatform/test  || true
+sleep 20000 || true
 sleep 3
 fab stop
 "
