3713e3a53185e1ee69c0f91f8683e7294d46c039
Matteo Nastasi <nastasi@openquake.org>
2017-08-24 20:15:52 +0200

    use ActionChains instead of raw commands to exploit strange selenium behavaior with panning map during isc_test

--- openquakeplatform/openquakeplatform/test/isc_test.py
+++ openquakeplatform/openquakeplatform/test/isc_test.py
@@ -3,16 +3,19 @@ import unittest
 import time
 
 from openquakeplatform.test import pla
+from selenium.webdriver.common.action_chains import ActionChains
 
 # @unittest.skip("temporarily disabled")
+
+
 class IscTest(unittest.TestCase):
     def isc_test(self):
         pla.get('/explore')
         pla.wait_new_page("//b[contains(text(), 'Seismic Hazard Data Sets and Models')]",
                           "/explore", strategy="next", timeout=10)
 
-        #<li>
-        #<a href="/maps/23">
+        #  <li>
+        #  <a href="/maps/23">
         enter_button = pla.xpath_finduniq(
             "//li/a[@href='/maps/23' and normalize-space(text()) = 'Global "
             "Instrumental Earthquake Catalogue (1900 - 2009)']")
@@ -32,20 +35,31 @@ class IscTest(unittest.TestCase):
             "= 'Identify']", 50)
         enter_button.click()
 
+        action = ActionChains(pla.driver)
+        action.move_by_offset(500, 400)
+        action.click_and_hold()
+        action.move_by_offset(150, 150)
+        action.release()
+        action.perform()
+
+        time.sleep(3)
+
         # wait info button will be clicked
         pla.xpath_finduniq(
             "//button[@type='button' and normalize-space(text())"
             "= 'Identify']/../../../../..[contains(concat(' ', @class, ' '),"
             " ' x-btn-pressed ')]", 100)
 
-        _, x, y = pla.xpath_finduniq_coords(
+        tile = pla.xpath_finduniq(
             "//img[contains(@src, 'wms?LAYERS=oqplatform%3Aisc_viewer_measure"
             "&FORMAT=image%2Fpng&TRANSPARENT=TRUE&SERVICE=WMS&VERSION=1.1.1"
             "&REQUEST=GetMap&STYLES=&TILED=true&SRS=EPSG%3A900913&BBOX="
             "-10018754.17,0,-5009377.085,5009377.085&WIDTH=256&HEIGHT=256')]",
             50)
 
-        pla.add_click_event()
-        pla.click_at(107 + x, 115 + y)
+        action = ActionChains(pla.driver)
+        action.move_to_element_with_offset(tile, 107, 115)
+        action.click()
+        action.perform()
 
         pla.xpath_finduniq("//div[text() = '1951-03-19T04:23:00']", 50)
