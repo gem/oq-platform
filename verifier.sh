66de013eb9eb294cadcdc1455b621a08e8a5d156
Matteo Nastasi <nastasi@openquake.org>
2017-11-21 18:25:29 +0100

    add creation of ipt server-side data folder for test purpose [4]

--- verifier.sh
+++ verifier.sh
@@ -669,11 +669,12 @@ export PYTHONPATH=\$(pwd):\$(pwd)/openquakeplatform/test/config
 ln -sf /etc/openquake/platform/local_settings.py openquakeplatform/
 
 # to be able to add files to server side IPT storage we change data folders permissions
+sudo chmod 775 /var/www/openquake/platform
+sudo chmod g+s /var/www/openquake/platform
 sudo mkdir -p /var/www/openquake/platform/data/1/ipt
-sudo chmod 775 \$(find /var/www/openquake/platform -type d)
-sudo chmod 664 \$(find /var/www/openquake/platform -type f)
-sudo chmod g+s \$(find /var/www/openquake/platform -type d)
-
+sudo chmod 775 \$(find /var/www/openquake/platform/data -type d)
+sudo chmod 664 \$(find /var/www/openquake/platform/data -type f)
+sudo chmod g+s \$(find /var/www/openquake/platform/data -type d)
 export GEM_OPT_PACKAGES=\"\$(python -c 'from openquakeplatform.settings import STANDALONE_APPS ; print(\",\".join(x for x in STANDALONE_APPS))')\"
 sed 's@^pla_basepath *= *\"http://localhost:8000\"@pla_basepath = \"http://oq-platform.localdomain\"@g' openquakeplatform/test/config/moon_config.py.tmpl > openquakeplatform/test/config/moon_config.py
 export DISPLAY=:1
