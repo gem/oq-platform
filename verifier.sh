ab0fddb114500f21c6c44c013cdc8af652625598
Matteo Nastasi <nastasi@openquake.org>
2017-11-26 21:09:02 +0100

    remove devel big sleep

--- verifier.sh
+++ verifier.sh
@@ -688,8 +688,8 @@ sleep 10
 export GEM_OPT_PACKAGES=\"openquakeplatform_ipt\"
 sed 's@^pla_basepath *= *\"http://localhost:8000\"@pla_basepath = \"http://oq-platform.localdomain\"@g' openquakeplatform/test/config/moon_config.py.tmpl > openquakeplatform/test/config/moon_config.py
 export DISPLAY=:1
-python -m openquake.moon.nose_runner --failurecatcher prod -v --with-xunit --xunit-file=xunit-platform-prod.xml openquakeplatform/test || true
-sleep 80000 || true
+python -m openquake.moon.nose_runner --failurecatcher prod -v --with-xunit --xunit-file=xunit-platform-prod.xml openquakeplatform/test # || true
+# sleep 80000 || true
 sleep 3
 cd -
 "
